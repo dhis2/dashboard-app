816be332a171d21931f66c6461dc5567ef93f46d
Jan Henrik Overland <jan@dhis2.org>
2017-09-29 11:02:20 +0200

    upgrade deps

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
 
 global.reportTablePlugin = {};
@@ -7,5 +8,10 @@ global.chartPlugin = {};
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>,
+        div
+    );
 });
