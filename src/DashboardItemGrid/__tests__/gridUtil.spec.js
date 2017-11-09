c743102c6111a8bd88d09c318bbb1a07b37f827f
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-11-09 13:34:35 +0100

    fix glob pattern for prettier

--- src/DashboardItemGrid/__tests__/gridUtil.spec.js
+++ src/DashboardItemGrid/__tests__/gridUtil.spec.js
@@ -1,33 +1,33 @@
-import { hasShape, getShape } from '../gridUtil';
+import { hasShape, getShape } from "../gridUtil";
 
-describe('gridUtil', () => {
-    describe('getShape', () => {
-        it('should get the position and size properties of the first grid block', () => {
-            expect(getShape(0)).toEqual({x: 0, y: 0, w: 9, h: 10});
-        });
-
-        it('should get the position and size properties of the eighth grid block', () => {
-            expect(getShape(7)).toEqual({ x: 9, y: 20, w: 9, h: 10 });
-        });
-
-        it('should throw an error when the grid block number is invalid', () => {
-            expect(() => {
-                getShape('octopus');
-            }).toThrow('Invalid grid block number');
-        });
+describe("gridUtil", () => {
+  describe("getShape", () => {
+    it("should get the position and size properties of the first grid block", () => {
+      expect(getShape(0)).toEqual({ x: 0, y: 0, w: 9, h: 10 });
     });
 
-    describe('hasShape', () => {
-        it('should return true if grid block object has correct properties', () => {
-            expect(hasShape({ x: 9, y: 20, w: 9, h: 10 })).toBeTruthy();
-        });
-
-        it('should return false if grid block object is missing properties', () => {
-            expect(hasShape({ x: 9, y: 20, w: 9 })).toBeFalsy();
-        });
-
-        it('should return false if grid block object has invalid properties', () => {
-            expect(hasShape({ x: 9, y: 20, w: 9, h: 'octopus' })).toBeFalsy();
-        });
+    it("should get the position and size properties of the eighth grid block", () => {
+      expect(getShape(7)).toEqual({ x: 9, y: 20, w: 9, h: 10 });
     });
+
+    it("should throw an error when the grid block number is invalid", () => {
+      expect(() => {
+        getShape("octopus");
+      }).toThrow("Invalid grid block number");
+    });
+  });
+
+  describe("hasShape", () => {
+    it("should return true if grid block object has correct properties", () => {
+      expect(hasShape({ x: 9, y: 20, w: 9, h: 10 })).toBeTruthy();
+    });
+
+    it("should return false if grid block object is missing properties", () => {
+      expect(hasShape({ x: 9, y: 20, w: 9 })).toBeFalsy();
+    });
+
+    it("should return false if grid block object has invalid properties", () => {
+      expect(hasShape({ x: 9, y: 20, w: 9, h: "octopus" })).toBeFalsy();
+    });
+  });
 });
--- src/reducers/__tests__/selectedDashboard.spec.js
+++ src/reducers/__tests__/selectedDashboard.spec.js
@@ -1,54 +1,60 @@
-import reducer,
-{ actionTypes, sGetSelectedDashboardFromState, sGetSelectedDashboardItems } from '../selectedDashboard';
+import reducer, {
+  actionTypes,
+  sGetSelectedDashboardFromState,
+  sGetSelectedDashboardItems
+} from "../selectedDashboard";
 
-describe('selected dashboard reducer', () => {
-    it('should set the selected dashboard', () => {
-        const selectedDash = 'my favorite dashboard';
-        const actualState = reducer(undefined, { type: actionTypes.SET_SELECTEDDASHBOARD, value: selectedDash });
-
-        expect(actualState).toEqual(selectedDash);
+describe("selected dashboard reducer", () => {
+  it("should set the selected dashboard", () => {
+    const selectedDash = "my favorite dashboard";
+    const actualState = reducer(undefined, {
+      type: actionTypes.SET_SELECTEDDASHBOARD,
+      value: selectedDash
     });
 
-    it('should return null as the default selected dashboard', () => {
-        const actualState = reducer(undefined, { type: 'NO_MATCH' });
+    expect(actualState).toEqual(selectedDash);
+  });
 
-        expect(actualState).toEqual(null);
+  it("should return null as the default selected dashboard", () => {
+    const actualState = reducer(undefined, { type: "NO_MATCH" });
+
+    expect(actualState).toEqual(null);
+  });
+
+  describe("selectors", () => {
+    it("should get the selected dashboard", () => {
+      const dash = { name: "my favorite dashboard" };
+      const state = {
+        selectedDashboard: dash
+      };
+
+      const actual = sGetSelectedDashboardFromState(state);
+      expect(actual).toEqual(dash);
     });
 
-    describe('selectors', () => {
-        it('should get the selected dashboard', () => {
-            const dash = {name: 'my favorite dashboard'};
-            const state = {
-                selectedDashboard: dash
-            };
+    it("should return an array of selected dashboard items", () => {
+      const dash = {
+        name: "my favorite dashboard",
+        dashboardItems: ["abc", "def"]
+      };
+      const state = {
+        selectedDashboard: dash
+      };
 
-            const actual = sGetSelectedDashboardFromState(state);
-            expect(actual).toEqual(dash);
-        });
-
-        it('should return an array of selected dashboard items', () => {
-            const dash = {
-                name: 'my favorite dashboard',
-                dashboardItems: ['abc', 'def'],
-            };
-            const state = {
-                selectedDashboard: dash
-            };
-
-            const actual = sGetSelectedDashboardItems(state);
-            expect(actual).toEqual(dash.dashboardItems);
-        });
-
-        it('should return an empty array if no dashboardItems', () => {
-            const dash = {
-                name: 'my favorite dashboard',
-            };
-            const state = {
-                selectedDashboard: dash
-            };
-
-            const actual = sGetSelectedDashboardItems(state);
-            expect(actual).toEqual([]);
-        });
+      const actual = sGetSelectedDashboardItems(state);
+      expect(actual).toEqual(dash.dashboardItems);
     });
+
+    it("should return an empty array if no dashboardItems", () => {
+      const dash = {
+        name: "my favorite dashboard"
+      };
+      const state = {
+        selectedDashboard: dash
+      };
+
+      const actual = sGetSelectedDashboardItems(state);
+      expect(actual).toEqual([]);
+    });
+  });
 });
