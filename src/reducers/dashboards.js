24912908dfe3d104b2d9836fad6779f5537a9661
Jen Jones Arnesen <jjarnesen@gmail.com>
2017-11-09 10:49:06 +0100

    Add jsdoc configuration and documentation for reducers (#9)

--- src/reducers/dashboards.js
+++ src/reducers/dashboards.js
@@ -1,11 +1,30 @@
+/** @module reducers/dashboards */
+
 import { validateReducer } from '../util';
 
+/**
+ * Action types for the dashboard reducer
+ * @constant
+ * @type {Object}
+ */
 export const actionTypes = {
     SET_DASHBOARDS: 'SET_DASHBOARDS',
 };
 
+/**
+ * The default list of dashboards
+ * @constant
+ * @type {Array}
+ */
 export const DEFAULT_DASHBOARDS = [];
 
+/**
+ * Reducer that computes and returns the new state based on the given action
+ * @function
+ * @param {Object} state The current state
+ * @param {Object} action The action to be evaluated
+ * @returns {Object}
+ */
 export default (state = DEFAULT_DASHBOARDS, action) => {
     switch (action.type) {
     case actionTypes.SET_DASHBOARDS:
@@ -15,16 +34,32 @@ export default (state = DEFAULT_DASHBOARDS, action) => {
     }
 };
 
-// root selector
+/**
+ * Returns the list of dashboards from the state object
+ * @function
+ * @param {Object} state The current state
+ * @returns {Array}
+ */
+export const sGetFromState = (state) => {
+    return state.dashboards;
+};
 
-export const sGetFromState = state => state.dashboards;
-
-// selectors level 1
-
+/**
+ * Returns a dashboard based on id, from the state object.
+ * If no matching dashboard is found, then undefined is returned
+ * @function
+ * @param {Object} state The current state
+ * @param {number} id The id of the dashboard to retrieve
+ * @returns {Object|undefined}
+ */
 export const sGetDashboardById = (state, id) => sGetFromState(state).find(dashboard => dashboard.id === id);
 
-// util
-
+/**
+ * Returns the array of dashboards, customized for ui
+ * @function
+ * @param {Array} data The original dashboard list
+ * @returns {Array}
+ */
 export const getDashboards = data => data.map(d => ({
     id: d.id,
     name: d.name,
@@ -36,8 +71,12 @@ export const getDashboards = data => data.map(d => ({
     numberOfItems: d.dashboardItems,
 }));
 
-// api
-
-export const getPersistedState = state => ({ // TODO
+/**
+ * Returns the persisted state
+ * @function
+ * @todo Implement the function
+ * @param {Object} state The current state
+ */
+export const getPersistedState = state => ({
     dashboards: state.dashboards,
 });
