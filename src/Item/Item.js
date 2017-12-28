1fb6450e347045739f6524eccbccdd0153151edc
Jen Jones Arnesen <jjarnesen@gmail.com>
2017-12-28 12:33:34 +0100

    Create separate Item component (#15)
    This is a work in progress. Plan is to move as much item-specific logic out of the ItemGrid, as possible. Item will most likely get connected directly to the redux store.

---
+++ src/Item/Item.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import ItemHeader from './ItemHeader';
+import ItemFooter from './ItemFooter';
+
+import { getFavoriteObjectFromItem } from '../ItemGrid/pluginUtil';
+
+const extractInterpretations = item => {
+    switch (item.type) {
+        case 'CHART':
+            return item.chart.interpretations;
+            break;
+        case 'REPORT_TABLE':
+            return item.reportTable.interpretations;
+            break;
+        default:
+            return [];
+            break;
+    }
+};
+
+class Item extends Component {
+    state = {
+        showInterpretations: false,
+    };
+
+    onToggleInterpretations = () => {
+        this.setState({ showInterpretations: !this.state.showInterpretations });
+        this.props.onToggleItemFooter(this.props.item.id);
+    };
+    render() {
+        const item = this.props.item;
+        const favorite = getFavoriteObjectFromItem(item);
+        const pluginId = `plugin-${getFavoriteObjectFromItem(item).id}`;
+
+        return (
+            <div>
+                <ItemHeader
+                    type={item.type}
+                    favoriteId={favorite.id}
+                    favoriteName={favorite.name}
+                    onButtonClick={this.props.onButtonClick}
+                    onInterpretationsClick={this.onToggleInterpretations}
+                />
+                <div id={pluginId} className="dashboard-item-content" />
+                <ItemFooter
+                    interpretations={extractInterpretations(item)}
+                    show={this.state.showInterpretations}
+                    onToggleInterpretations={this.onToggleInterpretations}
+                />
+            </div>
+        );
+    }
+}
+
+export default Item;
