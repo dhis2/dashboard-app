d63e9ed8f9221afda994a631bc1b14f29b20a8fd
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-01-05 23:53:39 +0100

    removed some duplicated code

--- src/Item/Interpretations/actions.js
+++ src/Item/Interpretations/actions.js
@@ -29,67 +29,50 @@ const onError = (action, error) => {
     return error;
 };
 
+const updateInterpretationInStore = (id, dispatch) => {
+    return getInterpretation(id).then(interpretation => {
+        return dispatch(addInterpretation(interpretation));
+    });
+};
+
 // thunks
 
 export const tLikeInterpretation = id => async dispatch => {
-    const onSuccess = res => {
-        return getInterpretation(id).then(interpretation => {
-            return dispatch(addInterpretation(interpretation));
-        });
-    };
-
     try {
-        const result = await postInterpretationLike(id);
-        return onSuccess(result);
+        await postInterpretationLike(id);
+        return updateInterpretationInStore(id, dispatch);
     } catch (err) {
         return onError('Like Interpretation', err);
     }
 };
 
 export const tUnlikeInterpretation = id => async dispatch => {
-    const onSuccess = res => {
-        return getInterpretation(id).then(interpretation => {
-            return dispatch(addInterpretation(interpretation));
-        });
-    };
-
     try {
-        const res = await deleteInterpretationLike(id);
-        return onSuccess(res);
+        await deleteInterpretationLike(id);
+        return updateInterpretationInStore(id, dispatch);
     } catch (err) {
         return onError('Unlike Interpretation', err);
     }
 };
 
 export const tAddInterpretationComment = data => async dispatch => {
-    const onSuccess = res => {
-        return getInterpretation(data.id).then(interpretation => {
-            return dispatch(addInterpretation(interpretation));
-        });
-    };
-
     try {
-        const res = await postInterpretationComment(data);
-        return onSuccess(res);
+        await postInterpretationComment(data);
+        return updateInterpretationInStore(data.id, dispatch);
     } catch (err) {
         return onError('Add Interpretation Comment', err);
     }
 };
 
 export const tDeleteInterpretationComment = data => async dispatch => {
-    const onSuccess = res => {
-        return getInterpretation(data.id).then(interpretation => {
-            return dispatch(addInterpretation(interpretation));
-        });
-    };
-
     try {
-        const res = await deleteInterpretationComment(data);
-        return onSuccess(res);
+        await deleteInterpretationComment(data);
+        return updateInterpretationInStore(data.id, dispatch);
     } catch (err) {
         return onError('Add Interpretation Comment', err);
     }
 };
+
 export const tGetInterpretations = ids => async dispatch => {
     const onSuccess = interpretations => {
         return dispatch(addInterpretations(interpretations));
