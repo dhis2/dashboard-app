f401765dd52bdea5283725b8ebcc1c22dd9dad68
Edoardo Sabadelli <edoardo@dhis2.org>
2018-01-12 13:33:20 +0100

    Add dashboard item selector component
    Made of a search field and a Popover where the results are listed

---
+++ src/ItemSelect/ItemSelect.js
@@ -0,0 +1,160 @@
+import React, { Fragment } from 'react';
+import Popover from 'material-ui/Popover';
+import TextField from 'material-ui/TextField';
+
+import { getInstance } from 'd2/lib/d2';
+import SvgIcon from 'd2-ui/lib/svg-icon/SvgIcon';
+import { itemTypeMap } from '../util';
+import ItemSelectList from './ItemSelectList';
+
+const ItemSearchField = props => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', width: '400px' }}>
+            <SvgIcon icon="Search" />
+            <TextField
+                hintText="Search favorite elements to add to dashboard"
+                type="text"
+                fullWidth={true}
+                onClick={props.onClick}
+                onChange={props.onChange}
+                style={{ marginLeft: '10px' }}
+            />
+        </div>
+    );
+};
+
+class ItemSelect extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            anchorEl: null,
+            open: false,
+            maxOptions: new Set(),
+            items: undefined,
+            filter: null,
+        };
+    }
+
+    closeList = () => {
+        this.setState({ open: false, filter: null });
+    };
+
+    openList = event => {
+        this.fetchItems();
+
+        this.setState({
+            anchorEl: event.currentTarget,
+            open: true,
+        });
+    };
+
+    setFilter = event => {
+        this.setState({ filter: event.target.value });
+
+        this.fetchItems();
+    };
+
+    fetchItems = async maxOption => {
+        const d2 = await getInstance();
+        const api = d2.Api.getApi();
+
+        let queryString;
+
+        if (maxOption) {
+            const maxOptions = this.state.maxOptions;
+
+            if (maxOptions.has(maxOption)) {
+                maxOptions.delete(maxOption);
+            } else {
+                maxOptions.add(maxOption);
+            }
+
+            this.setState({
+                maxOptions,
+            });
+        } else {
+            this.setState({
+                maxOptions: new Set(),
+            });
+        }
+
+        if ([...this.state.maxOptions.values()].length) {
+            queryString =
+                'max=' + [...this.state.maxOptions.values()].join('&max=');
+        }
+
+        api
+            .get(
+                `dashboards/q/${this.state.filter || ''}${
+                    queryString ? `?${queryString}` : ''
+                }`
+            )
+            .then(response => {
+                this.setState({ items: response });
+            })
+            .catch(console.error);
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <ItemSearchField
+                    onClick={this.openList}
+                    onChange={this.setFilter}
+                />
+                <Popover
+                    open={this.state.open}
+                    onRequestClose={this.closeList}
+                    anchorEl={this.state.anchorEl}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    canAutoPosition={false}
+                    autoCloseWhenOffScreen={false}
+                    style={{
+                        width: 700,
+                        height: 900,
+                        overflow: 'auto',
+                        left: -1000,
+                    }}
+                >
+                    {[
+                        { id: 'CHART', title: 'Charts' },
+                        { id: 'REPORT_TABLE', title: 'Pivot tables' },
+                        { id: 'MAP', title: 'Maps' },
+                        { id: 'EVENT_CHART', title: 'Event charts' },
+                        { id: 'EVENT_REPORT', title: 'Event reports' },
+                        { id: 'USER', title: 'Users' },
+                        { id: 'REPORT', title: 'Standard reports' },
+                        { id: 'RESOURCE', title: 'Resources' },
+                        { id: 'APP', title: 'Apps' },
+                    ].map(type => {
+                        const itemType = itemTypeMap[type.id];
+
+                        if (
+                            this.state.items &&
+                            this.state.items[itemType.countName] > 0
+                        ) {
+                            return (
+                                <ItemSelectList
+                                    key={type.id}
+                                    type={type.id}
+                                    title={type.title}
+                                    items={
+                                        this.state.items[itemType.endPointName]
+                                    }
+                                    onRequestAdd={this.closeList}
+                                    onChangeItemsLimit={this.fetchItems}
+                                />
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
+                </Popover>
+            </Fragment>
+        );
+    }
+}
+
+export default ItemSelect;
---
+++ src/ItemSelect/ItemSelectList.js
@@ -0,0 +1,215 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Divider from 'material-ui/Divider';
+import { List, ListItem } from 'material-ui/List';
+
+import Button from 'd2-ui/lib/button/Button';
+import SvgIcon from 'd2-ui/lib/svg-icon/SvgIcon';
+import { acAddDashboardItem } from '../actions';
+import { sGetSelectedDashboard } from '../reducers';
+import { sGetSelectedId } from '../reducers/selected';
+import { getYMax } from '../ItemGrid/gridUtil';
+
+const getIcon = type => {
+    switch (type) {
+        case 'APP':
+            return 'ViewList'; // XXX change to something app
+        case 'CHART':
+        case 'EVENT_CHART':
+            return 'InsertChart';
+        case 'REPORT_TABLE':
+        case 'EVENT_REPORT':
+        case 'MAP':
+            return 'Public';
+        case 'RESOURCE':
+            return 'ViewList'; // XXX change to something document
+        case 'REPORT':
+            return 'ViewList';
+        case 'USER':
+            return 'Person';
+        default:
+            return '';
+    }
+};
+
+class ItemSelectList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            seeMore: false,
+        };
+    }
+
+    getAppUrl = (type, id) => {
+        let appName;
+        let url;
+
+        switch (type) {
+            case 'APP':
+                url = '#';
+                break;
+            case 'CHART':
+                appName = 'dhis-web-visualizer/';
+                break;
+            case 'EVENT_CHART':
+                appName = 'dhis-web-event-visualizer/';
+                break;
+            case 'EVENT_REPORT':
+                appName = 'dhis-web-event-reports/';
+                break;
+            case 'MAP':
+                appName = 'dhis-web-mapping/';
+                break;
+            case 'REPORTS':
+                url = `../dhis-web-reporting/getReportParams.action?mode=report&uid=${id}`;
+                break;
+            case 'REPORT_TABLE':
+                appName = 'dhis-web-pivot/';
+                break;
+            case 'RESOURCES':
+                url = `${this.context.d2.Api.getApi().baseUrl}/documents/${id}`;
+                break;
+            case 'USERS':
+                appName = 'dhis-web-dashboard-integration/profile.action';
+                break;
+            default:
+                appName = '';
+                break;
+        }
+
+        if (!url && appName) {
+            // TODO getBaseUrl() from manifest?!
+            url = `../${appName}?id=${id}`;
+        }
+
+        return url;
+    };
+
+    addItem = item => () => {
+        const { dashboardId, dashboardItems } = this.props;
+        const yValue = getYMax(dashboardItems);
+
+        this.props.acAddDashboardItem(dashboardId, yValue, {
+            ...item,
+            type: this.props.type,
+        });
+
+        // TODO toggle the is this ok here?
+        this.props.onRequestAdd();
+    };
+
+    toggleSeeMore = () => {
+        this.setState({ seeMore: !this.state.seeMore });
+
+        this.props.onChangeItemsLimit(this.props.type);
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        paddingLeft: '16px',
+                    }}
+                >
+                    <h3>{this.props.title}</h3>
+                    <Button
+                        color="primary"
+                        style={{ textTransform: 'uppercase' }}
+                        onClick={this.toggleSeeMore}
+                    >
+                        {`See ${this.state.seeMore ? 'fewer' : 'more'} ${
+                            this.props.title
+                        }`}
+                    </Button>
+                </div>
+                <Divider />
+                <List>
+                    {this.props.items.map(item => (
+                        <ListItem
+                            key={item.id}
+                            leftIcon={
+                                <SvgIcon
+                                    icon={getIcon(this.props.type)}
+                                    style={{ margin: '6px' }}
+                                />
+                            }
+                            innerDivStyle={{ padding: '0px 0px 0px 42px' }}
+                            hoverColor="transparent"
+                            primaryText={
+                                <p
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-start',
+                                        margin: 0,
+                                    }}
+                                >
+                                    {item.displayName || item.name}
+                                    <Button
+                                        color="primary"
+                                        onClick={this.addItem(item)}
+                                    >
+                                        + ADD
+                                    </Button>
+                                    <a
+                                        href={this.getAppUrl(
+                                            this.props.type,
+                                            item.id
+                                        )}
+                                        style={{ display: 'flex' }}
+                                    >
+                                        <SvgIcon
+                                            icon="Launch"
+                                            style={{
+                                                width: '20px',
+                                                height: '20px',
+                                            }}
+                                        />
+                                    </a>
+                                </p>
+                            }
+                        />
+                    ))}
+                </List>
+            </Fragment>
+        );
+    }
+}
+
+ItemSelectList.propTypes = {
+    type: PropTypes.oneOf([
+        'CHART',
+        'EVENT_CHART',
+        'REPORT_TABLE',
+        'EVENT_REPORT',
+        'MAP',
+        'REPORTS',
+        'RESOURCES',
+        'USERS',
+    ]).isRequired,
+    title: PropTypes.string.isRequired,
+    items: PropTypes.array.isRequired,
+    onRequestAdd: PropTypes.func.isRequired,
+    onChangeItemsLimit: PropTypes.func.isRequired,
+    dashboardId: PropTypes.string.isRequired,
+    dashboardItems: PropTypes.array.isRequired,
+};
+
+ItemSelectList.contextTypes = {
+    d2: PropTypes.object.isRequired,
+};
+
+export default connect(
+    state => {
+        return {
+            dashboardId: sGetSelectedId(state),
+            dashboardItems: sGetSelectedDashboard(state).dashboardItems,
+        };
+    },
+    { acAddDashboardItem }
+)(ItemSelectList);
