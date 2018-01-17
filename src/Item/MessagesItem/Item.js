72f09abe1128d6c34382472cc5586b6e78907039
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-01-17 10:19:28 +0100

    Add All and Unread filter buttons

--- src/Item/MessagesItem/Item.js
+++ src/Item/MessagesItem/Item.js
@@ -3,11 +3,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import ItemHeader from '../ItemHeader';
+// import MessageItemHeaderButtons from './MessageItemHeaderButtons';
 import { fromMessages } from '../../reducers';
 import { colors } from '../../colors';
 import { formatDate, sortByDate } from '../../util';
 
+import './MessagesItem.css';
+
 const style = {
+    button: {
+        background: 'none !important',
+        border: 'none',
+        color: colors.darkGrey,
+        cursor: 'pointer',
+        font: 'inherit',
+        fontSize: '12px',
+        height: '14px',
+        lineJeight: '14px',
+        marginRight: '10px',
+        padding: '0 !important',
+    },
+    activeButton: {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+    },
     list: {
         listStyleType: 'none',
         paddingLeft: '0px',
@@ -45,6 +64,7 @@ const style = {
 class MessagesItem extends Component {
     state = {
         uiLocale: '',
+        filter: 'all',
     };
 
     async componentDidMount() {
@@ -61,41 +81,77 @@ class MessagesItem extends Component {
         }/dhis-web-messaging/readMessage.action?id=${id}`;
     };
 
+    filterAll = () => {
+        this.setState({ filter: 'all' });
+    };
+
+    filterUnread = () => {
+        this.setState({ filter: 'unread' });
+    };
+
     render() {
+        const activeStyle = Object.assign({}, style.button, style.activeButton);
+
+        const allButtonStyle =
+            this.state.filter === 'all' ? activeStyle : style.button;
+        const unreadButtonStyle =
+            this.state.filter === 'unread' ? activeStyle : style.button;
+
+        const actionButtons = !this.props.editMode ? (
+            <Fragment>
+                <button
+                    className="messages-action-button"
+                    type="button"
+                    style={allButtonStyle}
+                    onClick={this.filterAll}
+                >
+                    All
+                </button>
+                <button
+                    className="messages-action-button"
+                    type="button"
+                    style={unreadButtonStyle}
+                    onClick={this.filterUnread}
+                >
+                    Unread
+                </button>
+            </Fragment>
+        ) : null;
+
         const { messages } = this.props;
-        const messageItems = sortByDate(messages, 'lastUpdated', false).map(
-            msg => {
-                const listItemStyle = Object.assign({}, style.listitem, {
-                    fontWeight: msg.read ? 'normal' : 'bold',
-                });
-                return (
-                    <li style={listItemStyle} key={msg.id}>
-                        <div>
-                            <div style={style.author}>
-                                {msg.userFirstname} {msg.userSurname} ({
-                                    msg.messageCount
-                                })
-                            </div>
-                            <div style={style.date}>
-                                {formatDate(
-                                    msg.lastUpdated,
-                                    this.state.uiLocale
-                                )}
-                            </div>
-                            <a href={this.conversationHref(msg.id)}>
-                                <span style={style.title}>
-                                    {msg.displayName}
-                                </span>
-                            </a>
+        const filteredMessages = messages.filter(msg => {
+            return this.state.filter === 'unread' ? msg.read === false : true;
+        });
+        const messageItems = sortByDate(
+            filteredMessages,
+            'lastUpdated',
+            false
+        ).map(msg => {
+            const listItemStyle = Object.assign({}, style.listitem, {
+                fontWeight: msg.read ? 'normal' : 'bold',
+            });
+            return (
+                <li style={listItemStyle} key={msg.id}>
+                    <div>
+                        <div style={style.author}>
+                            {msg.userFirstname} {msg.userSurname} ({
+                                msg.messageCount
+                            })
                         </div>
-                    </li>
-                );
-            }
-        );
+                        <div style={style.date}>
+                            {formatDate(msg.lastUpdated, this.state.uiLocale)}
+                        </div>
+                        <a href={this.conversationHref(msg.id)}>
+                            <span style={style.title}>{msg.displayName}</span>
+                        </a>
+                    </div>
+                </li>
+            );
+        });
 
         return (
             <Fragment>
-                <ItemHeader title="Messages" />
+                <ItemHeader title="Messages" actionButtons={actionButtons} />
                 <hr style={style.line} />
                 <div className="dashboard-item-content">
                     <ul style={style.list}>{messageItems}</ul>
