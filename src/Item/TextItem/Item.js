b2271f4475db007bcd877e3f001cb818595399ce
Edoardo Sabadelli <edoardo@dhis2.org>
2018-01-24 09:41:49 +0100

    Add text dashboard item

---
+++ src/Item/TextItem/Item.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+
+import TextField from 'd2-ui/lib/text-field/TextField';
+import { acUpdateDashboardItem } from '../../actions/editDashboard';
+import { sGetEditDashboard } from '../../reducers/editDashboard';
+
+const TextItem = props => {
+    const { item, editMode, text, acUpdateDashboardItem } = props;
+
+    const onChangeText = text => {
+        console.log('change text');
+        const updatedItem = {
+            ...item,
+            text,
+        };
+
+        acUpdateDashboardItem(updatedItem);
+    };
+
+    return (
+        <Fragment>
+            {editMode ? (
+                <TextField
+                    value={text}
+                    placeholder={'Add text here'}
+                    onChange={onChangeText}
+                />
+            ) : (
+                text
+            )}
+        </Fragment>
+    );
+};
+
+export default connect(
+    (state, ownProps) => ({
+        text: sGetEditDashboard(state).dashboardItems.find(
+            item => item.id === ownProps.item.id
+        ).text,
+    }),
+    {
+        acUpdateDashboardItem,
+    }
+)(TextItem);
