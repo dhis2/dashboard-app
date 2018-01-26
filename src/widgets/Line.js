852116e26ffb390865ab5b9896ec4fe1ec8ad3a4
Jen Jones Arnesen <jjarnesen@gmail.com>
2018-01-26 08:47:11 +0100

    Move List item content delete icon next to resource name (#60)
    The reason for moving the icon to be just to the right of the resource name is so that it isn't so close to the delete icon for the entire item. Also, made it smaller to further distinguish it from the Item delete button. A horizontal line was also added to separate the header from the content area of the item, similar to the messages type.

---
+++ src/widgets/Line.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { colors } from '../colors';
+
+const style = {
+    backgroundColor: `${colors.lightGrey}`,
+    border: 'none',
+    height: '1px',
+    margin: '0px 0px 5px 0px',
+};
+
+const Line = props => <hr style={style} />;
+
+export default Line;
