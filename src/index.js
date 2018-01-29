45669316b5fd67aae111cc4c4020a95fb061409c
Edoardo Sabadelli <edoardo@dhis2.org>
2018-01-29 13:53:32 +0100

    Initialize translations engine and add source

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import D2UIApp from 'd2-ui/lib/app/D2UIApp';
 
-import { getManifest } from 'd2/lib/d2';
+import { config, getManifest, getUserSettings } from 'd2/lib/d2';
 
 import './index.css';
 
@@ -16,31 +16,47 @@ import App from './App';
 // init material-ui
 injectTapEventPlugin();
 
+let baseUrl;
+
+const configI18n = userSettings => {
+    const uiLocale = userSettings.keyUiLocale;
+
+    if (uiLocale && uiLocale !== 'en') {
+        config.i18n.sources.add(`./i18n/i18n_module_${uiLocale}.properties`);
+    }
+
+    config.i18n.sources.add('./i18n/i18n_module_en.properties');
+};
+
 // init d2
-getManifest('manifest.webapp').then(manifest => {
-    const baseUrl =
-        process.env.NODE_ENV === 'production'
-            ? manifest.getBaseUrl()
-            : 'http://localhost:8080';
-    ReactDOM.render(
-        <D2UIApp
-            initConfig={{
-                baseUrl: `${baseUrl}/api`,
-                headers: { Authorization: `Basic ${btoa('admin:district')}` },
-                schemas: [
-                    'dashboard',
-                    'chart',
-                    'reportTable',
-                    'map',
-                    'eventReport',
-                    'eventChart',
-                ],
-            }}
-        >
-            <Provider store={configureStore()}>
-                <App baseUrl={baseUrl} />
-            </Provider>
-        </D2UIApp>,
-        document.getElementById('root')
-    );
-});
+getManifest('manifest.webapp')
+    .then(manifest => {
+        baseUrl =
+            process.env.NODE_ENV === 'production'
+                ? manifest.getBaseUrl()
+                : 'http://localhost:8080';
+
+        config.baseUrl = `${baseUrl}/api`;
+    })
+    .then(getUserSettings)
+    .then(configI18n)
+    .then(() => {
+        config.headers = { Authorization: `Basic ${btoa('admin:district')}` };
+        config.schemas = [
+            'dashboard',
+            'chart',
+            'reportTable',
+            'map',
+            'eventReport',
+            'eventChart',
+        ];
+
+        ReactDOM.render(
+            <D2UIApp initConfig={config}>
+                <Provider store={configureStore()}>
+                    <App baseUrl={baseUrl} />
+                </Provider>
+            </D2UIApp>,
+            document.getElementById('root')
+        );
+    });
