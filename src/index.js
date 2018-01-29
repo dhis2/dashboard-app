f4d27ce0cb01d2da255fb7474f5784b466cd58a3
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-01-29 13:53:59 +0100

    Get baseUrl and authorization from developer config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* global DHIS_CONFIG */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -21,12 +23,18 @@ getManifest('manifest.webapp').then(manifest => {
     const baseUrl =
         process.env.NODE_ENV === 'production'
             ? manifest.getBaseUrl()
-            : 'http://localhost:8080';
+            : DHIS_CONFIG.baseUrl;
+
+    const headers =
+        process.env.NODE_ENV !== 'production' && DHIS_CONFIG.authorization
+            ? { Authorization: DHIS_CONFIG.authorization }
+            : null;
+
     ReactDOM.render(
         <D2UIApp
             initConfig={{
                 baseUrl: `${baseUrl}/api`,
-                headers: { Authorization: `Basic ${btoa('admin:district')}` },
+                headers,
                 schemas: [
                     'dashboard',
                     'chart',
