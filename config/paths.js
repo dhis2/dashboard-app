3a0a0530470c86d1fd237084f53ac379f5d86ebb
Jan Henrik Øverland <jan@dhis2.org>
2018-01-30 23:57:02 +0100

    Change served path

--- config/paths.js
+++ config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : './');
   return ensureSlash(servedUrl, true);
 }
 
@@ -53,3 +53,5 @@ module.exports = {
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
 };
+
+console.log("EXPORTS", module.exports);
