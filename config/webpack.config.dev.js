e21016af73665b4d8abe433c165fe924c1357f2c
Jan Henrik Øverland <jan@dhis2.org>
2018-01-31 18:21:06 +0100

    Remove react aliases + Add d2 alias

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -112,8 +112,6 @@ module.exports = {
             // Support React Native Web
             // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
             'react-native': 'react-native-web',
-            react: path.resolve('node_modules/react'),
-            'react-dom': path.resolve('node_modules/react-dom'),
             d2: path.resolve('node_modules/d2'),
         },
         plugins: [
