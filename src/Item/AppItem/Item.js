880f1596c846775a1eda565ea12c05ebb4494395
Edoardo Sabadelli <edoardo@dhis2.org>
2018-06-19 15:37:29 +0200

    Remove shouldComponentUpdate() as the component is connected
    No need for using a class either

--- src/Item/AppItem/Item.js
+++ src/Item/AppItem/Item.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -9,74 +9,66 @@ import SvgIcon from 'd2-ui/lib/svg-icon/SvgIcon';
 import { fromItemFilter } from '../../reducers';
 import { FILTER_USER_ORG_UNIT } from '../../actions/itemFilter';
 
-class AppItem extends Component {
-    shouldComponentUpdate(nextProps) {
-        return nextProps.itemFilter !== this.props.itemFilter;
-    }
+const AppItem = ({ item, itemFilter }, context) => {
+    let appDetails;
 
-    render() {
-        const { item, itemFilter } = this.props;
+    const appKey = item.appKey;
 
-        let appDetails;
-
-        const appKey = item.appKey;
-
-        if (appKey) {
-            appDetails = this.context.d2.system.installedApps.find(
-                app => app.folderName === appKey
-            );
-        }
-
-        let iframeSrc = `${appDetails.launchUrl}?dashboardItemId=${item.id}`;
-
-        if (
-            itemFilter &&
-            itemFilter[FILTER_USER_ORG_UNIT] &&
-            itemFilter[FILTER_USER_ORG_UNIT].length
-        ) {
-            const ouIds = itemFilter[FILTER_USER_ORG_UNIT].map(
-                ouPath => ouPath.split('/').slice(-1)[0]
-            );
-
-            iframeSrc += `&userOrgUnit=${ouIds.join(',')}`;
-        }
-
-        return appDetails && appDetails.name && appDetails.launchUrl ? (
-            <Fragment>
-                <ItemHeader title={appDetails.name} />
-                <Line />
-                <iframe
-                    title={appDetails.name}
-                    src={iframeSrc}
-                    className="dashboard-item-content"
-                    style={{
-                        border: 'none',
-                    }}
-                />
-            </Fragment>
-        ) : (
-            <Fragment>
-                <ItemHeader title={`${appKey} app not found`} />
-                <Line />
-                <div
-                    className="dashboard-item-content"
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '90%',
-                    }}
-                >
-                    <SvgIcon
-                        icon="NotInterested"
-                        style={{ width: 100, height: 100, align: 'center' }}
-                        disabled
-                    />
-                </div>
-            </Fragment>
+    if (appKey) {
+        appDetails = context.d2.system.installedApps.find(
+            app => app.folderName === appKey
         );
     }
-}
+
+    let iframeSrc = `${appDetails.launchUrl}?dashboardItemId=${item.id}`;
+
+    if (
+        itemFilter &&
+        itemFilter[FILTER_USER_ORG_UNIT] &&
+        itemFilter[FILTER_USER_ORG_UNIT].length
+    ) {
+        const ouIds = itemFilter[FILTER_USER_ORG_UNIT].map(
+            ouPath => ouPath.split('/').slice(-1)[0]
+        );
+
+        iframeSrc += `&userOrgUnit=${ouIds.join(',')}`;
+    }
+
+    return appDetails && appDetails.name && appDetails.launchUrl ? (
+        <Fragment>
+            <ItemHeader title={appDetails.name} />
+            <Line />
+            <iframe
+                title={appDetails.name}
+                src={iframeSrc}
+                className="dashboard-item-content"
+                style={{
+                    border: 'none',
+                }}
+            />
+        </Fragment>
+    ) : (
+        <Fragment>
+            <ItemHeader title={`${appKey} app not found`} />
+            <Line />
+            <div
+                className="dashboard-item-content"
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '90%',
+                }}
+            >
+                <SvgIcon
+                    icon="NotInterested"
+                    style={{ width: 100, height: 100, align: 'center' }}
+                    disabled
+                />
+            </div>
+        </Fragment>
+    );
+};
 
 AppItem.contextTypes = {
     d2: PropTypes.object,
