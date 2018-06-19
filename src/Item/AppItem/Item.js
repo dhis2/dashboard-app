a6e2a5ca5e0fa8662521a21b06bd1a5dacc739c6
Edoardo Sabadelli <edoardo@dhis2.org>
2018-06-19 14:09:51 +0200

    DHIS2-4056 Pass dashboard filters in the App's iframe URL

--- src/Item/AppItem/Item.js
+++ src/Item/AppItem/Item.js
@@ -1,59 +1,89 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import ItemHeader from '../ItemHeader';
 import Line from '../../widgets/Line';
 import SvgIcon from 'd2-ui/lib/svg-icon/SvgIcon';
 
-const AppItem = ({ item }, context) => {
-    let appDetails;
+import { fromItemFilter } from '../../reducers';
+import { FILTER_USER_ORG_UNIT } from '../../actions/itemFilter';
 
-    const appKey = item.appKey;
-
-    if (appKey) {
-        appDetails = context.d2.system.installedApps.find(
-            app => app.folderName === appKey
-        );
+class AppItem extends Component {
+    shouldComponentUpdate(nextProps) {
+        return nextProps.itemFilter !== this.props.itemFilter;
     }
 
-    return appDetails && appDetails.name && appDetails.launchUrl ? (
-        <Fragment>
-            <ItemHeader title={appDetails.name} />
-            <Line />
-            <iframe
-                title={appDetails.name}
-                src={`${appDetails.launchUrl}?dashboardItemId=${item.id}`}
-                className="dashboard-item-content"
-                style={{
-                    border: 'none',
-                }}
-            />
-        </Fragment>
-    ) : (
-        <Fragment>
-            <ItemHeader title={`${appKey} app not found`} />
-            <Line />
-            <div
-                className="dashboard-item-content"
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '90%',
-                }}
-            >
-                <SvgIcon
-                    icon="NotInterested"
-                    style={{ width: 100, height: 100, align: 'center' }}
-                    disabled
+    render() {
+        const { item, itemFilter } = this.props;
+
+        let appDetails;
+
+        const appKey = item.appKey;
+
+        if (appKey) {
+            appDetails = this.context.d2.system.installedApps.find(
+                app => app.folderName === appKey
+            );
+        }
+
+        let iframeSrc = `${appDetails.launchUrl}?dashboardItemId=${item.id}`;
+
+        if (
+            itemFilter &&
+            itemFilter[FILTER_USER_ORG_UNIT] &&
+            itemFilter[FILTER_USER_ORG_UNIT].length
+        ) {
+            const ouIds = itemFilter[FILTER_USER_ORG_UNIT].map(
+                ouPath => ouPath.split('/').slice(-1)[0]
+            );
+
+            iframeSrc += `&userOrgUnit=${ouIds.join(',')}`;
+        }
+
+        return appDetails && appDetails.name && appDetails.launchUrl ? (
+            <Fragment>
+                <ItemHeader title={appDetails.name} />
+                <Line />
+                <iframe
+                    title={appDetails.name}
+                    src={iframeSrc}
+                    className="dashboard-item-content"
+                    style={{
+                        border: 'none',
+                    }}
                 />
-            </div>
-        </Fragment>
-    );
-};
+            </Fragment>
+        ) : (
+            <Fragment>
+                <ItemHeader title={`${appKey} app not found`} />
+                <Line />
+                <div
+                    className="dashboard-item-content"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '90%',
+                    }}
+                >
+                    <SvgIcon
+                        icon="NotInterested"
+                        style={{ width: 100, height: 100, align: 'center' }}
+                        disabled
+                    />
+                </div>
+            </Fragment>
+        );
+    }
+}
 
 AppItem.contextTypes = {
     d2: PropTypes.object,
 };
 
-export default AppItem;
+const mapStateToProps = state => ({
+    itemFilter: fromItemFilter.sGetFromState(state),
+});
+
+export default connect(mapStateToProps)(AppItem);
