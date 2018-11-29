30497dd08086d1add0f1fcc711c623758a2a8f4b
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-29 14:56:51 +0100

    Disable toggle buttons for YOY chart types (#211)
    Toggling from chart to pivot table or maps does not work currently for YOY chart types.
    Until we decide if/how to handle the switch we disable the toggle buttons only for YOY charts.
    
    (cherry picked from commit f9f1c9630a4e66c44d7f0145fa5a0e18522c44a9)

--- src/Item/ItemHeaderButton.js
+++ src/Item/ItemHeaderButton.js
@@ -3,9 +3,11 @@ import SvgIcon from 'd2-ui/lib/svg-icon/SvgIcon';
 
 import './ItemHeaderButton.css';
 
-const ItemHeaderButton = ({ icon, onClick, style = {} }) => {
+const ItemHeaderButton = ({ icon, disabled, onClick, style = {} }) => {
     return (
         <button
+            disabled={disabled}
+            type="button"
             className="item-action-button"
             style={style.container}
             onClick={onClick}
