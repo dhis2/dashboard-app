7ad8f89f1f35da0428bdb0a86dea55794931f506
Edoardo Sabadelli <edoardo@dhis2.org>
2019-01-23 11:49:45 +0100

    DHIS2-4008: fix interpretation/comment sorting
    Also, do not pass translated field name to the sorting function.
    As soon as a language different than English is used and there are
    translations for it, the sorting breaks.

--- src/components/Item/VisualizationItem/Interpretations/Interpretation.js
+++ src/components/Item/VisualizationItem/Interpretations/Interpretation.js
@@ -264,7 +264,7 @@ class Interpretation extends Component {
 
         const comments = sortByDate(
             this.props.interpretation.comments,
-            i18n.t('created')
+            'created'
         ).map(comment => (
             <li
                 className="comment-container"
