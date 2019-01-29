60deccb5fb36b89ae7c2a6488fea2231a5eaae44
Jen Jones Arnesen <jennifer@dhis2.org>
2019-01-29 09:43:48 +0100

    test cleanup

--- src/components/TitleBar/__tests__/EditTitleBar.spec.js
+++ src/components/TitleBar/__tests__/EditTitleBar.spec.js
@@ -6,41 +6,31 @@ jest.mock('d2-ui/lib/text-field/TextField', () => 'textfield');
 jest.mock('../../ItemSelect/ItemSelect', () => 'itemselect');
 
 describe('EditTitleBar', () => {
+    const props = {
+        name: 'Rainbow Dash',
+        description: 'The blue one',
+        onChangeTitle: Function.prototype,
+        onChangeDescription: Function.prototype,
+        style: {
+            title: { xyz: '890' },
+            description: { abc: '123' },
+        },
+        classes: {
+            section: { sectionStyle: 'section' },
+            titleDescription: { titleDescStyle: 'titleDesc' },
+            title: { titleStyle: 'title' },
+            itemSelect: { itemSelStyle: 'itemSel' },
+        },
+    };
+
     it('renders correctly when displayName not provided', () => {
-        const tree = renderer
-            .create(
-                <EditTitleBar
-                    name="Rainbow Dash"
-                    description="The blue one"
-                    onChangeTitle={Function.prototype}
-                    onChangeDescription={Function.prototype}
-                    style={{
-                        title: { xyz: '890' },
-                        description: { abc: '123' },
-                    }}
-                    classes={{}}
-                />
-            )
-            .toJSON();
+        const tree = renderer.create(<EditTitleBar {...props} />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('renders correctly when displayName is provided', () => {
         const tree = renderer
-            .create(
-                <EditTitleBar
-                    name="Rainbow Dash"
-                    displayName="Regnbue Dash"
-                    description="The blue one"
-                    onChangeTitle={Function.prototype}
-                    onChangeDescription={Function.prototype}
-                    style={{
-                        title: { xyz: '890' },
-                        description: { abc: '123' },
-                    }}
-                    classes={{}}
-                />
-            )
+            .create(<EditTitleBar displayName="Regnbue Dash" {...props} />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
