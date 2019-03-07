4a0dc463006aff5b5df0d1a01a982888cbc39e0a
Jen Jones Arnesen <jennifer@dhis2.org>
2019-03-07 10:05:10 +0100

    regenerate yarn

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,7 +4,6 @@ const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
@@ -50,16 +49,6 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 
 // Note: defined here because it will be used more than once.
 const cssFilename = 'static/css/[name].[contenthash:8].css';
-
-// // ExtractTextPlugin expects the build output to be flat.
-// // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
-// // However, our output is structured with css, js and media folders.
-// // To have this structure working with relative paths, we have to use custom options.
-// const extractTextPluginOptions = shouldUseRelativeAssetPaths
-//     ? // Making sure that the publicPath goes back to to build folder.
-//       { publicPath: Array(cssFilename.split('/').length).join('../') }
-//     : {};
-
 const scriptPrefix = '..';
 
 // This is the production configuration.
@@ -157,8 +146,6 @@ module.exports = {
             },
             {
                 oneOf: [
-                    // "url" loader works just like "file" loader but it also embeds
-                    // assets smaller than specified size as data URLs to avoid requests.
                     {
                         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
                         loader: require.resolve('url-loader'),
@@ -176,18 +163,6 @@ module.exports = {
                             compact: true,
                         },
                     },
-                    // The notation here is somewhat confusing.
-                    // "postcss" loader applies autoprefixer to our CSS.
-                    // "css" loader resolves paths in CSS and adds assets as dependencies.
-                    // "style" loader normally turns CSS into JS modules injecting <style>,
-                    // but unlike in development configuration, we do something different.
-                    // `ExtractTextPlugin` first applies the "postcss" and "css" loaders
-                    // (second argument), then grabs the result CSS and puts it into a
-                    // separate file in our build process. This way we actually ship
-                    // a single CSS file in production instead of JS code injecting <style>
-                    // tags. If you use code splitting, however, any async bundles will still
-                    // use the "style" loader inside the async code so CSS from them won't be
-                    // in the main CSS file.
                     {
                         test: /\.css$/,
                         use: [
@@ -223,10 +198,6 @@ module.exports = {
                     },
                     {
                         loader: require.resolve('file-loader'),
-                        // Exclude `js` files to keep "css" loader working as it injects
-                        // it's runtime that would otherwise processed through "file" loader.
-                        // Also exclude `html` and `json` extensions so they get processed
-                        // by webpacks internal loaders.
                         exclude: [/\.js$/, /\.html$/, /\.json$/],
                         options: {
                             name: 'static/media/[name].[hash:8].[ext]',
@@ -284,11 +255,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: cssFilename,
         }),
-
-        // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-        // new ExtractTextPlugin({
-        //     filename: cssFilename,
-        // }),
 
         // Generate a manifest file which contains a mapping of all asset filenames
         // to their corresponding output file so that tools can pick it up without
