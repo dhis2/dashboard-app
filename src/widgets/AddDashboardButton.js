642c582aac9eb11c4f84a5e4487060937d690bab
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-02 22:17:19 +0200

    style jsx

--- src/widgets/AddDashboardButton.js
+++ src/widgets/AddDashboardButton.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import AddIcon from '@material-ui/icons/AddCircle';
 import { colors } from '../modules/colors';
-import { NONAME } from 'dns';
 
-const styles = {
-    button: {
-        width: 36,
-        height: 36,
-        padding: 0,
-        backgroundColor: 'transparent',
-        border: 'none',
-        boxShadow: 'none',
-        // cursor: 'pointer',
-    },
-    icon: {
-        fill: colors.mediumGreen,
-        // color: colors.mediumGreen,
-        width: '1.3em',
-        height: '1.3em',
-        // cursor: 'pointer',
-    },
-};
-
-const AddButton = () => {
+const AddDashboardButton = () => {
     return (
-        <button style={styles.button}>
-            <AddIcon style={styles.icon} />
-        </button>
+        <React.Fragment>
+            <button className="add-dashboard-button">
+                <AddIcon className="add-dashboard-icon" />
+            </button>
+
+            <style jsx>{`
+                .add-dashboard-button {
+                    width: 36px;
+                    height: 36px;
+                    padding: 0;
+                    background-color: transparent;
+                    border: none;
+                    outline: none;
+                    box-shadow: none;
+                }
+
+                .add-dashboard-button:focus {
+                    outline: none;
+                }
+
+                .add-dashboard-icon {
+                    fill: ${colors.mediumGreen};
+                    width: 1.3em;
+                    height: 1.3em;
+                    cursor: pointer;
+                }
+            `}</style>
+        </React.Fragment>
     );
 };
 
-export default AddButton;
+export default AddDashboardButton;
