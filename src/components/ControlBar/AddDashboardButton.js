1025375248cb46e8fe2cf6b4ee036771e85194b5
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-06 10:50:50 +0200

    dont need the cursor, since the wrapping anchor element has it by default

--- src/components/ControlBar/AddDashboardButton.js
+++ src/components/ControlBar/AddDashboardButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './styles/AddDashboardButton.module.css';
 import { colors } from '../../modules/colors';
 
 const AddDashboardButton = () => (
@@ -9,7 +8,6 @@ const AddDashboardButton = () => (
         height="32"
         fill={colors.mediumGreen}
         viewBox="0 0 24 24"
-        className={styles.icon}
     >
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 11h-3v3c0 .55-.45 1-1 1s-1-.45-1-1v-3H8c-.55 0-1-.45-1-1s.45-1 1-1h3V8c0-.55.45-1 1-1s1 .45 1 1v3h3c.55 0 1 .45 1 1s-.45 1-1 1z" />
     </svg>
