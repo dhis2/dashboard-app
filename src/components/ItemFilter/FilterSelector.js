d4a711ee15d0d81c621eae295fd061cc2781f2d0
Edoardo Sabadelli <edoardo@dhis2.org>
2019-05-07 08:35:21 +0200

    fix(styles): fix styles for filter selector
    Open the filter dialogs within the app container for inheriting the app's
    styles.
    Fixes DHIS2-6804.

--- src/components/ItemFilter/FilterSelector.js
+++ src/components/ItemFilter/FilterSelector.js
@@ -125,6 +125,7 @@ class FilterSelector extends Component {
                     anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
                     onClose={this.closePanel}
                     style={{ height: '100%' }}
+                    container={document.querySelector('#app-wrapper')}
                 >
                     <DimensionsPanel
                         dimensions={dimensions}
