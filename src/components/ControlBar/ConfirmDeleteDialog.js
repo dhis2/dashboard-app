3f2037dfa2a3a96f95bcedb3a9b2899210fd3259
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-07 12:46:48 +0200

    use new mui dialog

--- src/components/ControlBar/ConfirmDeleteDialog.js
+++ src/components/ControlBar/ConfirmDeleteDialog.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import i18n from '@dhis2/d2-i18n';
-import Dialog from 'material-ui/Dialog';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import { Button } from '@dhis2/ui-core';
 
 export const ConfirmDeleteDialog = ({
@@ -10,27 +14,28 @@ export const ConfirmDeleteDialog = ({
     open,
 }) => {
     const actions = [
-        <span style={{ marginRight: '15px' }}>
-            <Button primary onClick={onDeleteConfirmed}>
-                {i18n.t('Delete')}
-            </Button>
-        </span>,
-        <Button secondary onClick={onContinueEditing}>
-            {i18n.t('Continue editing')}
+        <Button key="cancel" secondary onClick={onContinueEditing}>
+            {i18n.t('Cancel')}
+        </Button>,
+        <Button key="delete" destructive onClick={onDeleteConfirmed}>
+            {i18n.t('Delete')}
         </Button>,
     ];
 
     return (
-        <Dialog
-            title={i18n.t('Confirm delete dashboard')}
-            actions={actions}
-            modal={true}
-            open={open}
-        >
-            {i18n.t(
-                'Are you sure you want to delete dashboard "{{ dashboardName }}"?',
-                { dashboardName }
-            )}
+        <Dialog open={open} onClose={onContinueEditing}>
+            <DialogTitle style={{ fontSize: '1.25rem', fontWeight: '450' }}>
+                {i18n.t('Delete dashboard')}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText style={{ fontSize: '0.875rem' }}>
+                    {i18n.t(
+                        'Deleting dashboard "{{ dashboardName }}" will remove it for all users. This action cannot be undone. Are you sure you want to permanently delete this dashboard?',
+                        { dashboardName }
+                    )}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>{actions}</DialogActions>
         </Dialog>
     );
 };
--- src/components/ControlBar/__tests__/ConfirmDeleteDialog.spec.js
+++ src/components/ControlBar/__tests__/ConfirmDeleteDialog.spec.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Dialog from 'material-ui/Dialog';
 import { Button } from '@dhis2/ui-core';
-
+import DialogActions from '@material-ui/core/DialogActions';
 import { ConfirmDeleteDialog } from '../ConfirmDeleteDialog';
 import { getStubContext } from '../../../setupTests';
 
@@ -28,56 +27,34 @@ describe('ConfirmDeleteDialog', () => {
         shallowDialog = undefined;
     });
 
-    it('renders a Dialog', () => {
-        expect(dialog().find(Dialog).length).toBeGreaterThan(0);
-    });
-
-    it('renders two buttons', () => {
-        expect(dialog().prop('actions')).toHaveLength(2);
-    });
-
     it('matches the snapshot', () => {
         expect(dialog()).toMatchSnapshot();
     });
 
-    it.skip('renders a Button with action onContinueEditing', () => {
-        // expect.assertions(1);
+    it('renders a Button with action onContinueEditing', () => {
+        expect.assertions(1);
         dialog()
-            .prop('actions')
+            .find(DialogActions)
+            .children()
             .forEach(actionEl => {
-                const element = shallow(actionEl);
-
-                if (!element.prop('style')) {
-                    console.log(
-                        'element.childAt(0).props',
-                        element.childAt(0).props()
+                if (actionEl.key() === 'cancel') {
+                    expect(actionEl.prop('onClick')).toBe(
+                        props.onContinueEditing
                     );
-
-                    expect(element.find(Button)).toHaveLength(1);
-
-                    // expect(element.childAt(0).prop('onClick')).toBe(
-                    //     props.onContinueEditing
-                    // );
                 }
             });
     });
 
-    it.skip('renders a Button with action onDeleteConfirmed', () => {
-        // expect.assertions(1);
+    it('renders a Button with action onDeleteConfirmed', () => {
+        expect.assertions(1);
         dialog()
-            .prop('actions')
+            .find(DialogActions)
+            .children()
             .forEach(actionEl => {
-                console.log('here', actionEl.type);
-
-                if (actionEl.type === 'span') {
-                    console.log('yes it is a span');
-
-                    const element = shallow(actionEl);
-                    console.log('element', element);
-
-                    // expect(actionEl.childAt(0).prop('onClick')).toEqual(
-                    //     props.onDeleteConfirmed
-                    // );
+                if (actionEl.key() === 'delete') {
+                    expect(actionEl.prop('onClick')).toBe(
+                        props.onDeleteConfirmed
+                    );
                 }
             });
     });
