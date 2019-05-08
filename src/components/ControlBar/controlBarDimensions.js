5c5973ed95edb8130e16bd884bc6279f603889ef
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-08 10:49:31 +0200

    fix: set filter badge top position based on offset height (#301) (#303)
    Fixes [DHIS2-6631]
    
    The top position of the filter badges should be based on the height of the dashboards bar, so that the dashboards bar doesn't hide the badge if its been configured to more than one row

--- src/components/ControlBar/controlBarDimensions.js
+++ src/components/ControlBar/controlBarDimensions.js
@@ -3,9 +3,9 @@ import { SHOWMORE_BAR_HEIGHT } from './ShowMoreButton';
 
 export const CONTROL_BAR_ROW_HEIGHT = 36;
 export const FIRST_ROW_PADDING_HEIGHT = 10;
-export const HEADERBAR_HEIGHT = 48;
-
 export const MIN_ROW_COUNT = 1;
+
+const HEADERBAR_HEIGHT = 48;
 
 const CONTROL_BAR_OUTER_HEIGHT_DIFF =
     FIRST_ROW_PADDING_HEIGHT + SHOWMORE_BAR_HEIGHT - 2; // 2 pixel "adjustment"
@@ -18,6 +18,10 @@ export const getNumRowsFromHeight = height => {
     return Math.floor(
         (height - CONTROL_BAR_OUTER_HEIGHT_DIFF) / CONTROL_BAR_ROW_HEIGHT
     );
+};
+
+export const getTopOffset = rows => {
+    return HEADERBAR_HEIGHT + getControlBarHeight(rows, false);
 };
 
 export const getControlBarHeight = (rows, expandable) => {
