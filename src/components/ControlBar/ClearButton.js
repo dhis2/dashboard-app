fd1912128b93fd562958ac3036ae6499ed2875c7
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-10 11:18:19 +0200

    remove the mui icon

--- src/components/ControlBar/ClearButton.js
+++ src/components/ControlBar/ClearButton.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IconClear from '@material-ui/icons/Clear';
+import ClearIcon from '../../icons/Clear';
 
 import classes from './styles/ClearButton.module.css';
 
-export const ClearButton = ({ onChange }) => {
-    return (
-        <button className={classes.clearbutton} onClick={onChange}>
-            <IconClear
-                style={{ width: '16px', height: '16px' }}
-                color="action"
-            />
-        </button>
-    );
-};
+export const ClearButton = ({ onChange }) => (
+    <button className={classes.button} onClick={onChange}>
+        <ClearIcon className={classes.icon} color="action" />
+    </button>
+);
 
 ClearButton.propTypes = {
     onChange: PropTypes.func.isRequired,
