d49922aca41a6e34f2d7c66e07c0f65f3e2abff9
Edoardo Sabadelli <edoardo@dhis2.org>
2019-09-04 14:02:06 +0200

    Backport epi weeks and DV plugin fixes (#333)
    * feat: Enable epi-weekly periods in filter
    
    * Enable epi-weekly periods in filter
    
    * Fix dev authorization credentials
    
    (cherry picked from commit c3d6afc8b465617f22d598ae2e71030d152fa102)
    
    * fix: upgrade DV plugin for single value support
    
    (cherry picked from commit 0b643c701cd9a01c3a6c6e43bd9d8a9ecad6da2e)

--- src/reducers/dimensions.js
+++ src/reducers/dimensions.js
@@ -1,4 +1,4 @@
-import { FIXED_DIMENSIONS } from '@dhis2/d2-ui-analytics';
+import { FIXED_DIMENSIONS } from '@dhis2/analytics';
 
 export const SET_DIMENSIONS = 'SET_DIMENSIONS';
 export const DEFAULT_DIMENSIONS = {
