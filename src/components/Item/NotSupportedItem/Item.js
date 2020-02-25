98a53f68d54d4f8bbef560528f4a048403f23e55
Jen Jones Arnesen <jennifer@dhis2.org>
2020-02-25 07:25:25 -0800

    fix: interpolation issue

--- src/components/Item/NotSupportedItem/Item.js
+++ src/components/Item/NotSupportedItem/Item.js
@@ -1,15 +1,14 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import i18n from '@dhis2/d2-i18n';
-
 import ItemHeader from '../ItemHeader';
 import NotInterestedIcon from '@material-ui/icons/NotInterested';
 
 const NotSupportedItem = props => (
     <Fragment>
         <ItemHeader
-            title={i18n.t('Item type not supported: {{itemType}}', {
-                itemType: props.item.type,
+            title={i18n.t('Item type "{{type}}" not supported', {
+                type: props.item.type,
             })}
             itemId={props.item.id}
         />
