c8ac9ff65d5c17a17ce8920a4e80baa89e59af96
Jen Jones Arnesen <jennifer@dhis2.org>
2020-03-20 08:24:28 -0700

    fix: calculate item height accounting for long title [DHIS2-8492] (#664)
    fixes: https://jira.dhis2.org/browse/DHIS2-8492
    
    Calculate the height of the item based on the header height - which is no longer constant since items now support wrapped titles.
    
    This calculation is the backup value incase the measured height of the chart is not available right away, as in view mode.
    
    In addition, fn getContentStyle was memoized to reduce chart rerenders when items are moved in edit mode.

--- src/components/Item/ItemHeader.js
+++ src/components/Item/ItemHeader.js
@@ -8,6 +8,9 @@ import DeleteItemButton from './DeleteItemButton';
 
 import classes from './styles/ItemHeader.module.css';
 
+// This is the margin-top + margin-bottom defined in the css file
+export const HEADER_MARGIN_HEIGHT = 12;
+
 const ItemHeader = props => {
     const {
         title,
@@ -15,12 +18,13 @@ const ItemHeader = props => {
         actionButtons,
         itemId,
         acRemoveDashboardItem,
+        forwardedRef,
     } = props;
 
     const handleDeleteItem = () => acRemoveDashboardItem(itemId);
 
     return (
-        <div className={classes.itemHeaderWrap}>
+        <div className={classes.itemHeaderWrap} ref={forwardedRef}>
             <p className={classes.itemTitle}>{title}</p>
             {editMode ? (
                 <div className={classes.itemActionsWrap}>
@@ -41,8 +45,13 @@ ItemHeader.propTypes = {
     acRemoveDashboardItem: PropTypes.func,
     actionButtons: PropTypes.node,
     editMode: PropTypes.bool,
+    forwardedRef: PropTypes.object,
     itemId: PropTypes.string,
     title: PropTypes.string,
+};
+
+ItemHeader.defaultProps = {
+    forwardedRef: {},
 };
 
 const mapStateToProps = state => ({
@@ -53,7 +62,15 @@ const mapDispatchToProps = {
     acRemoveDashboardItem,
 };
 
-export default connect(
+const ConnectedItemHeader = connect(
     mapStateToProps,
     mapDispatchToProps
 )(ItemHeader);
+
+// TODO this is a false positive that is fixed in eslint-plugin-react v7.15
+// github.com/yannickcr/eslint-plugin-react/blob/master/CHANGELOG.md
+/* eslint-disable react/display-name */
+export default React.forwardRef((props, ref) => (
+    <ConnectedItemHeader {...props} forwardedRef={ref} />
+));
+/* eslint-enable react/display-name */
