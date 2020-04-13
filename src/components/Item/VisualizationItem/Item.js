51216c98c464794baaf757fc818cfc82de36381b
Jan Henrik Øverland <jan@dhis2.org>
2020-04-13 23:02:15 +0200

    fix: hide titles for maps (DHIS2-8616) (#718)
    * fix: support options and pass hideTitle for maps
    
    * fix: hide map title also when switching from another type

--- src/components/Item/VisualizationItem/Item.js
+++ src/components/Item/VisualizationItem/Item.js
@@ -235,6 +235,11 @@ export class Item extends Component {
                     );
                 }
 
+                props.options = {
+                    ...props.options,
+                    hideTitle: true,
+                };
+
                 return <DefaultPlugin {...props} />;
             }
             default: {
