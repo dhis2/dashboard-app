1e7c97b2c2113f22715e88971f26190c4b166c4e
Jen Jones Arnesen <jennifer@dhis2.org>
2020-05-12 16:25:50 -0700

    fix: use functions to get current translation

--- src/reducers/dimensions.js
+++ src/reducers/dimensions.js
@@ -1,10 +1,23 @@
 import { FIXED_DIMENSIONS } from '@dhis2/analytics';
 
+const getDefaultDimensions = () =>
+    Object.keys(FIXED_DIMENSIONS)
+        .filter(dimId =>
+            [FIXED_DIMENSIONS.pe.id, FIXED_DIMENSIONS.ou.id].includes(dimId)
+        )
+        .reduce((acc, key) => {
+            const dimObj = {
+                id: FIXED_DIMENSIONS[key].id,
+                iconName: FIXED_DIMENSIONS[key].iconName,
+                name: FIXED_DIMENSIONS[key].name(),
+            };
+            acc[key] = dimObj;
+            return acc;
+        }, {});
+
 export const SET_DIMENSIONS = 'SET_DIMENSIONS';
-export const DEFAULT_DIMENSIONS = {
-    pe: FIXED_DIMENSIONS.pe,
-    ou: FIXED_DIMENSIONS.ou,
-};
+
+const DEFAULT_DIMENSIONS = getDefaultDimensions();
 
 export default (state = DEFAULT_DIMENSIONS, action) => {
     switch (action.type) {
