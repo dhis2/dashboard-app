e3af7d5de97ca276ab78036906864dc5ce401e94
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-11 11:13:51 +0200

    fix: move print buttons to More menu

--- src/components/TitleBar/ViewTitleBar.js
+++ src/components/TitleBar/ViewTitleBar.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import i18n from '@dhis2/d2-i18n'
+import { Redirect } from 'react-router-dom'
 import SharingDialog from '@dhis2/d2-ui-sharing-dialog'
 import Star from '@material-ui/icons/Star'
 import StarBorder from '@material-ui/icons/StarBorder'
+import Popover from '@material-ui/core/Popover'
+import { Button, Menu, MenuItem, colors } from '@dhis2/ui'
 
+import { ThreeDots } from '../Item/VisualizationItem/assets/icons'
 import { orObject } from '../../modules/util'
 import { tStarDashboard } from '../../actions/dashboards'
 import { acSetSelectedShowDescription } from '../../actions/selected'
 import FilterSelector from '../ItemFilter/FilterSelector'
-import { Button, colors } from '@dhis2/ui'
 import Info from './Info'
 import {
     sGetSelectedId,
@@ -26,113 +29,127 @@ import classes from './styles/ViewTitleBar.module.css'
 
 const NO_DESCRIPTION = i18n.t('No description')
 
-class ViewTitleBar extends Component {
-    constructor(props) {
-        super(props)
+const ViewTitleBar = (props, context) => {
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [sharingDialogIsOpen, setSharingDialogIsOpen] = useState(false)
+    const [redirectUrl, setRedirectUrl] = useState(null)
 
-        this.state = {
-            sharingDialogIsOpen: false,
-        }
+    const toggleSharingDialog = () =>
+        setSharingDialogIsOpen(!sharingDialogIsOpen)
+
+    const openMoreOptions = (_, event) => setAnchorEl(event.currentTarget)
+
+    const closeMoreOptions = () => setAnchorEl(null)
+
+    const printLayout = () => setRedirectUrl(`${props.id}/printlayout`)
+    const printOipp = () => setRedirectUrl(`${props.id}/printoipp`)
+
+    const {
+        id,
+        name,
+        description,
+        access,
+        style,
+        showDescription,
+        starred,
+        onStarClick,
+        onInfoClick,
+    } = props
+
+    const titleStyle = Object.assign({}, style.title, {
+        cursor: 'default',
+        userSelect: 'text',
+        top: '7px',
+    })
+
+    const StarIcon = starred ? Star : StarBorder
+
+    if (redirectUrl) {
+        return <Redirect to={redirectUrl} />
     }
 
-    toggleSharingDialog = () =>
-        this.setState({ sharingDialogIsOpen: !this.state.sharingDialogIsOpen })
-
-    render() {
-        const {
-            id,
-            name,
-            description,
-            access,
-            style,
-            showDescription,
-            starred,
-            onStarClick,
-            onInfoClick,
-        } = this.props
-
-        const titleStyle = Object.assign({}, style.title, {
-            cursor: 'default',
-            userSelect: 'text',
-            top: '7px',
-        })
-
-        const StarIcon = starred ? Star : StarBorder
-
-        return (
-            <>
-                <div className={classes.titleBar}>
-                    <span style={titleStyle}>{name}</span>
-                    <div className={classes.actions}>
-                        <div
-                            className={classes.titleBarIcon}
-                            onClick={onStarClick}
-                        >
-                            <StarIcon style={{ fill: colors.grey600 }} />
-                        </div>
-                        <div className={classes.titleBarIcon}>
-                            <Info onClick={onInfoClick} />
-                        </div>
-                        <div style={{ marginLeft: '10px' }}>
-                            {access.update ? (
-                                <Link
-                                    className={classes.editLink}
-                                    to={`/${id}/edit`}
-                                >
-                                    <Button>{i18n.t('Edit')}</Button>
-                                </Link>
-                            ) : null}
-                            {access.manage ? (
-                                <span style={{ marginRight: '4px' }}>
-                                    <Button onClick={this.toggleSharingDialog}>
-                                        {i18n.t('Share')}
-                                    </Button>
-                                </span>
-                            ) : null}
-                            <span style={{ marginRight: '4px' }}>
-                                <FilterSelector />
+    return (
+        <>
+            <div className={classes.titleBar}>
+                <span style={titleStyle}>{name}</span>
+                <div className={classes.actions}>
+                    <div className={classes.titleBarIcon} onClick={onStarClick}>
+                        <StarIcon style={{ fill: colors.grey600 }} />
+                    </div>
+                    <div className={classes.titleBarIcon}>
+                        <Info onClick={onInfoClick} />
+                    </div>
+                    <div className={classes.strip}>
+                        {access.update ? (
+                            <Link
+                                className={classes.editLink}
+                                to={`/${id}/edit`}
+                            >
+                                <Button>{i18n.t('Edit')}</Button>
+                            </Link>
+                        ) : null}
+                        {access.manage ? (
+                            <Button onClick={toggleSharingDialog}>
+                                {i18n.t('Share')}
+                            </Button>
+                        ) : null}
+                        <FilterSelector />
+                        <Button onClick={openMoreOptions}>
+                            <ThreeDots />
+                            <span style={{ marginLeft: '5px' }}>
+                                {i18n.t('More')}
                             </span>
-                            <Link
-                                className={classes.editLink}
-                                to={`/${id}/printoipp`}
-                            >
-                                <Button>
-                                    {i18n.t('Print one item per page')}
-                                </Button>
-                            </Link>
-                            <Link
-                                className={classes.editLink}
-                                to={`/${id}/printlayout`}
-                            >
-                                <Button>{i18n.t('Print layout')}</Button>
-                            </Link>
-                        </div>
+                        </Button>
                     </div>
+                    {anchorEl && (
+                        <Popover
+                            open={Boolean(anchorEl)}
+                            onClose={closeMoreOptions}
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                        >
+                            <Menu>
+                                <MenuItem
+                                    dense
+                                    label={i18n.t('Print layout')}
+                                    onClick={printLayout}
+                                />
+                                <MenuItem
+                                    dense
+                                    label={i18n.t('Print one item per page')}
+                                    onClick={printOipp}
+                                />
+                            </Menu>
+                        </Popover>
+                    )}
                 </div>
-                {showDescription ? (
-                    <div
-                        className="dashboard-description"
-                        style={Object.assign(
-                            { paddingTop: '10px', paddingBottom: '5px' },
-                            style.description,
-                            !description ? { color: '#888' } : {}
-                        )}
-                    >
-                        {description || NO_DESCRIPTION}
-                    </div>
-                ) : null}
-                {id ? (
-                    <SharingDialog
-                        d2={this.context.d2}
-                        id={id}
-                        type="dashboard"
-                        open={this.state.sharingDialogIsOpen}
-                        onRequestClose={this.toggleSharingDialog}
-                    />
-                ) : null}
-            </>
-        )
-    }
+            </div>
+            {showDescription ? (
+                <div
+                    className="dashboard-description"
+                    style={Object.assign(
+                        { paddingTop: '5px', paddingBottom: '5px' },
+                        style.description,
+                        !description ? { color: '#888' } : {}
+                    )}
+                >
+                    {description || NO_DESCRIPTION}
+                </div>
+            ) : null}
+            {id ? (
+                <SharingDialog
+                    d2={context.d2}
+                    id={id}
+                    type="dashboard"
+                    open={sharingDialogIsOpen}
+                    onRequestClose={toggleSharingDialog}
+                />
+            ) : null}
+        </>
+    )
 }
 
 ViewTitleBar.propTypes = {
