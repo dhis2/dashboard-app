a519120c35a03541b4d7e2eb441b7e52b95188e8
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-12 17:50:37 +0200

    fix: use 3rd party adapter for d2 and old i18n config

--- src/AppWrapper.spec.js
+++ src/AppWrapper.spec.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import AppWrapper from './AppWrapper'
 
 jest.mock('./components/App', () => () => <div />) // eslint-disable-line react/display-name
+jest.mock('@dhis2/app-runtime-adapter-d2', () => {
+    return {
+        D2Shim: ({ children }) => children({ d2: {} }),
+    }
+})
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
