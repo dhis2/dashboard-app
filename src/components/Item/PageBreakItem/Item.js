16d717951afb4bdc925d26f7417d3524841c546d
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-18 18:56:03 +0200

    fix: start of making pages look like real pages

--- src/components/Item/PageBreakItem/Item.js
+++ src/components/Item/PageBreakItem/Item.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import i18n from '@dhis2/d2-i18n'
 
-import classes from './styles/PageBreakItem.module.css'
-
-const PageBreakItem = () => (
-    <p className={classes.text}>
-        {i18n.t('Page break marker - will not appear on the printed dashboard')}
-    </p>
-)
+const PageBreakItem = () => <div />
 
 export default PageBreakItem
