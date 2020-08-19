5eb4635c2846b9464bcbdaeeaa3f127ba41f1822
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-19 17:28:57 +0200

    fix: add missing period

--- src/components/Dashboard/PrintInfo.js
+++ src/components/Dashboard/PrintInfo.js
@@ -18,7 +18,7 @@ const PrintInfo = ({ type }) => {
             </p>
             <p className={classes.info}>
                 {i18n.t(
-                    'The pages below are a preview of how the dashboard items will be printed on a single page. Use the default printer settings for best results. This text will not be printed'
+                    'The pages below are a preview of how the dashboard items will be printed on a single page. Use the default printer settings for best results. This text will not be printed.'
                 )}
             </p>
             <hr className={classes.divider} />
