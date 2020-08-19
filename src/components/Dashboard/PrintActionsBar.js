3ea7dafea384aa6465ba3a122e5711af54226c78
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-19 09:03:55 +0200

    fix: add < icon to the Exit print preview btn

--- src/components/Dashboard/PrintActionsBar.js
+++ src/components/Dashboard/PrintActionsBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import i18n from '@dhis2/d2-i18n'
 import { Button } from '@dhis2/ui'
 import { Link } from 'react-router-dom'
+import LessHorizontalIcon from '../../icons/LessHorizontal'
 import { a4LandscapeWidthPx } from '../../modules/printUtils'
 
 import classes from './PrintActionsBar.module.css'
@@ -17,7 +18,10 @@ const PrintActionsBar = ({ id }) => {
         <div className={classes.container}>
             <div className={classes.inner} style={{ width }}>
                 <Link className={classes.link} to={`/${id}`}>
-                    <Button>{i18n.t('Exit print preview')}</Button>
+                    <Button>
+                        <LessHorizontalIcon />
+                        {i18n.t('Exit print preview')}
+                    </Button>
                 </Link>
                 <Button onClick={window.print}>{i18n.t('Print')}</Button>
             </div>
