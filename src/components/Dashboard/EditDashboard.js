5c737908a22d1273f70f8da93df40c9bd9cfe2e5
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-24 09:37:46 +0200

    fix: use semantic naming

--- src/components/Dashboard/EditDashboard.js
+++ src/components/Dashboard/EditDashboard.js
@@ -7,7 +7,7 @@ import DashboardVerticalOffset from './DashboardVerticalOffset'
 import TitleBar from '../TitleBar/TitleBar'
 import ItemGrid from '../ItemGrid/ItemGrid'
 import EditBar from '../ControlBar/EditBar'
-import PrintLayoutDashboard from './PrintLayoutDashboard'
+import LayoutPrintPreview from './PrintLayoutDashboard'
 import NoContentMessage from '../../widgets/NoContentMessage'
 import { acSetEditDashboard } from '../../actions/editDashboard'
 import { sGetSelectedId } from '../../reducers/selected'
@@ -40,16 +40,16 @@ export class EditDashboard extends Component {
     }
 
     renderGrid = () => {
-        if (!this.props.isPrintPreviewView) {
-            return (
-                <>
-                    <TitleBar edit={true} />
-                    <ItemGrid edit={true} />
-                </>
-            )
-        } else {
-            return <PrintLayoutDashboard fromEdit={true} />
+        if (this.props.isPrintPreviewView) {
+            return <LayoutPrintPreview fromEdit={true} />
         }
+
+        return (
+            <div className="dashboard-wrapper">
+                <TitleBar edit={true} />
+                <ItemGrid edit={true} />
+            </div>
+        )
     }
 
     render() {
@@ -62,7 +62,6 @@ export class EditDashboard extends Component {
                 ) : (
                     <NoContentMessage text={i18n.t('No access')} />
                 )}
-                <div className="dashboard-wrapper">{}</div>
             </>
         )
     }
