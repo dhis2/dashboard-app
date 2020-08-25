f49e771d8e1a4166f76151f4b6f1fec72dfa03fd
Jen Jones Arnesen <jennifer@dhis2.org>
2020-08-25 08:51:33 +0200

    chore: tests for ViewDashboard

--- src/components/Dashboard/__tests__/ViewDashboard.spec.js
+++ src/components/Dashboard/__tests__/ViewDashboard.spec.js
@@ -1,92 +1,51 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { ViewDashboard, Content } from '../ViewDashboard'
-import { NoContentMessage } from '../../../widgets/NoContentMessage'
+import toJson from 'enzyme-to-json'
+import { ViewDashboard } from '../ViewDashboard'
 
-jest.mock('../DashboardContent', () => () => <div />) // eslint-disable-line react/display-name
-jest.mock('../../ControlBar/DashboardsBar', () => () => <div />) // eslint-disable-line react/display-name
-jest.mock('../DashboardVerticalOffset', () => () => <div />) // eslint-disable-line react/display-name
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useEffect: f => f(),
+}))
+
+jest.mock('../../ControlBar/DashboardsBar', () => 'DashboardsBar')
+jest.mock('../DashboardVerticalOffset', () => 'DashboardVerticalOffset')
+jest.mock('../../TitleBar/TitleBar', () => 'TitleBar')
+jest.mock('../../FilterBar/FilterBar', () => 'FilterBar')
+jest.mock('../../ItemGrid/ItemGrid', () => 'ItemGrid')
 
 describe('ViewDashboard', () => {
     let props
-    let shallowViewDashboard
-    const viewDashboard = () => {
-        if (!shallowViewDashboard) {
-            shallowViewDashboard = shallow(<ViewDashboard {...props} />)
-        }
-        return shallowViewDashboard
-    }
-
-    const assertContent = hasContent => {
-        const content = viewDashboard().find(Content)
-
-        expect(content.length).toBe(1)
-        expect(content.dive().find(NoContentMessage)).toHaveLength(
-            hasContent ? 0 : 1
-        )
-    }
 
     beforeEach(() => {
         props = {
-            id: undefined,
-            dashboardsIsEmpty: undefined,
-            dashboardsLoaded: undefined,
+            clearEditDashboard: jest.fn(),
+            clearPrintDashboard: jest.fn(),
+            dashboardIsEditing: false,
+            dashboardIsPrinting: false,
         }
-        shallowViewDashboard = undefined
     })
 
-    describe('when "dashboardsLoaded" is false', () => {
-        it('does not render any children inside the div', () => {
-            props.dashboardsLoaded = false
-
-            expect(
-                viewDashboard()
-                    .find('.dashboard-wrapper')
-                    .children().length
-            ).toBe(0)
-        })
+    afterEach(() => {
+        jest.clearAllMocks()
     })
 
-    describe('when "dashboardsLoaded" is true', () => {
-        beforeEach(() => {
-            props.dashboardsLoaded = true
-        })
+    it('renders correctly default', () => {
+        const tree = shallow(<ViewDashboard {...props} />)
+        expect(toJson(tree)).toMatchSnapshot()
+    })
 
-        describe('when "id" is null', () => {
-            it('does not render any children inside the div', () => {
-                props.id = null
-                expect(
-                    viewDashboard()
-                        .find('.dashboard-wrapper')
-                        .children().length
-                ).toBe(0)
-            })
-        })
+    it('clears edit dashboard after redirecting from Edit mode', () => {
+        props.dashboardIsEditing = true
+        shallow(<ViewDashboard {...props} />)
+        expect(props.clearEditDashboard).toHaveBeenCalled()
+        expect(props.clearPrintDashboard).not.toHaveBeenCalled()
+    })
 
-        describe('when "dashboardsIsEmpty" is true', () => {
-            beforeEach(() => {
-                props.dashboardsIsEmpty = true
-            })
-
-            it('renders a NoContentMessage', () => {
-                assertContent(false)
-            })
-        })
-
-        describe('when "dashboardsIsEmpty" is false', () => {
-            beforeEach(() => {
-                props.dashboardsIsEmpty = false
-            })
-
-            describe('when id is not null', () => {
-                beforeEach(() => {
-                    props.id = '123xyz'
-                })
-
-                it('renders DashboardContent', () => {
-                    assertContent(true)
-                })
-            })
-        })
+    it('clears print dashboard after redirecting from Print mode', () => {
+        props.dashboardIsPrinting = true
+        shallow(<ViewDashboard {...props} />)
+        expect(props.clearEditDashboard).not.toHaveBeenCalled()
+        expect(props.clearPrintDashboard).toHaveBeenCalled()
     })
 })
