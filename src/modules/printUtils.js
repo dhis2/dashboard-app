2c49410b5e1e96b43624c2b9f1a9c6d56b4589e9
Jen Jones Arnesen <jennifer@dhis2.org>
2020-09-08 09:56:38 +0200

    fix: include title page in edit preview but scroll to below it [DHIS2-9417] (#1038)
    In contrast to the Print Layout (from View mode), the Edit print preview did not have a title page, which means there was no page break prior to the first item page. This means the first item page had more vertical space in Edit print preview than in Print Layout. The result could lead to more items getting on the first page in Edit preview, than the actual print.
    
    The solution is to include the title page in Edit preview as well, so that Edit preview and Print Layout are identical. Then, to allow the user to focus on the important stuff (item print layout), the Edit preview is auto scrolled to the top of the first item page.

--- src/modules/printUtils.js
+++ src/modules/printUtils.js
@@ -9,6 +9,22 @@ export const a4LandscapeWidthPx = 1102
 
 export const MAX_ITEM_GRID_HEIGHT = 33
 
+export const getTransformYPx = elStyle => {
+    if (!elStyle || !elStyle.transform) {
+        return null
+    }
+
+    // find valid transforms - those with y pixels
+    // the code is expecting the transform prop to
+    // look like:  translate(10px, 300px)
+    const transformY = elStyle.transform.split(' ')[1]?.match(/(\d+)px/)
+    if (transformY) {
+        return parseInt(transformY[1])
+    } else {
+        return null
+    }
+}
+
 export const getDomGridItemsSortedByYPos = elements => {
     const types = Object.keys(itemTypeMap)
     const elementsWithBoundingRect = orArray(elements).map(el => {
@@ -17,10 +33,7 @@ export const getDomGridItemsSortedByYPos = elements => {
         )
 
         const rect = el.getBoundingClientRect()
-
-        const y = el.style.transform
-            ? parseInt(el.style.transform?.split(' ')[1]?.slice(0, -3))
-            : parseInt(rect.y)
+        const y = getTransformYPx(el.style) || parseInt(rect.y)
 
         return {
             type,
