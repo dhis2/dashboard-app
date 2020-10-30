d4dc9636789e2d1940717f53548851a2c3fa7fb2
Jen Jones Arnesen <jennifer@dhis2.org>
2020-10-30 11:06:26 +0100

    test: upgrade to latest cypress-commands and README

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,7 @@
-import '@dhis2/cli-utils-cypress/support'
+import { registerCommands } from '@dhis2/cypress-commands'
 
 import { enableNetworkShim, loginAndPersistSession } from './server'
 
+registerCommands()
 enableNetworkShim()
 loginAndPersistSession()
