2df5b2620b06553bd5436671d864da2375f9c0bb
Jen Jones Arnesen <jennifer@dhis2.org>
2020-11-04 12:26:03 +0100

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,5 @@
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
-    extends: [config.eslintReact],
-    globals: {
-        Cypress: 'readonly',
-        after: 'readonly',
-        before: 'readonly',
-        cy: 'readonly',
-    },
+    extends: [config.eslintReact, 'plugin:cypress/recommended'],
 }
