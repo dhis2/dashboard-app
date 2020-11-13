848dae2f09777b3f2ae6f8955af6f77a5b5d2fde
Edoardo Sabadelli <edoardo@dhis2.org>
2020-11-13 16:44:23 +0100

    refactor: use dataEngine in place of d2 for api CRUD operations

--- src/api/dashboards.js
+++ src/api/dashboards.js
@@ -1,55 +1,97 @@
-import { getInstance } from 'd2'
 import arrayClean from 'd2-utilizr/lib/arrayClean'
 import { onError, getDashboardFields } from './index'
 
-// Get "all" dashboards on startup
-export const apiFetchDashboards = () =>
-    getInstance()
-        .then(d2 =>
-            d2.models.dashboard.list({
-                fields: [
-                    getDashboardFields().join(','),
-                    'dashboardItems[id]',
-                ].join(','),
-                paging: 'false',
-            })
-        )
-        .catch(onError)
-
-// Get more info about selected dashboard
-export const apiFetchDashboard = id =>
-    getInstance()
-        .then(d2 =>
-            d2.models.dashboard.get(id, {
-                fields: arrayClean(
-                    getDashboardFields({
-                        withItems: true,
-                        withFavorite: { withDimensions: false },
-                    })
-                ).join(','),
-            })
-        )
-        .catch(onError)
-
-// Star dashboard
-export const apiStarDashboard = (id, isStarred) => {
-    const url = `dashboards/${id}/favorite`
-
-    getInstance().then(d2 => {
-        if (isStarred) {
-            d2.Api.getApi().post(url)
-        } else {
-            d2.Api.getApi().delete(url)
-        }
-    })
+export const dashboardsQuery = {
+    resource: 'dashboards',
+    params: {
+        fields: [getDashboardFields(), 'dashboardItems[id]'].join(','),
+        paging: false,
+    },
 }
 
-export const apiDeleteDashboard = id => {
-    return getInstance()
-        .then(d2 => {
-            return d2.models.dashboards
-                .get(id)
-                .then(dashboard => dashboard.delete())
+export const dashboardQuery = {
+    resource: 'dashboards',
+    id: ({ id }) => id,
+    params: {
+        fields: arrayClean(
+            getDashboardFields({
+                withItems: true,
+                withFavorite: { withDimensions: false },
+            })
+        ).join(','),
+    },
+}
+
+export const starDashboardMutation = {
+    type: 'create',
+    resource: 'dashboards',
+    id: ({ id }) => `${id}/favorite`,
+}
+
+export const unstarDashboardMutation = {
+    type: 'delete',
+    resource: 'dashboards',
+    id: ({ id }) => `${id}/favorite`,
+}
+
+export const deleteDashboardMutation = {
+    type: 'delete',
+    resource: 'dashboards',
+    id: ({ id }) => id,
+}
+
+// Get "all" dashboards on startup
+export const apiFetchDashboards = async dataEngine => {
+    try {
+        const dashboardsData = await dataEngine.query({
+            dashboards: dashboardsQuery,
         })
-        .catch(onError)
+
+        return dashboardsData.dashboards.dashboards
+    } catch (error) {
+        onError(error)
+    }
+}
+
+// Get more info about selected dashboard
+export const apiFetchDashboard = async (dataEngine, id) => {
+    try {
+        const dashboardData = await dataEngine.query(
+            { dashboard: dashboardQuery },
+            {
+                variables: {
+                    id,
+                },
+            }
+        )
+
+        return dashboardData.dashboard
+    } catch (error) {
+        onError(error)
+    }
+}
+
+// Star dashboard
+export const apiStarDashboard = async (dataEngine, id, isStarred) => {
+    try {
+        if (isStarred) {
+            await dataEngine.mutate(starDashboardMutation, {
+                variables: { id },
+            })
+        } else {
+            await dataEngine.mutate(unstarDashboardMutation, {
+                variables: { id },
+            })
+        }
+    } catch (error) {
+        onError(error)
+    }
+}
+
+export const apiDeleteDashboard = async (dataEngine, id) => {
+    try {
+        await dataEngine.mutate(deleteDashboardMutation, { variables: { id } })
+    } catch (error) {
+        onError(error)
+    }
 }
