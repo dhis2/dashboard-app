855fe957f09e3187fe3abed907e8be73e8a5d688
Jen Jones Arnesen <jennifer@dhis2.org>
2020-11-17 11:33:35 +0100

    fix: revert Text item changes

--- src/components/Item/TextItem/Item.js
+++ src/components/Item/TextItem/Item.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
-import cx from 'classnames'
-import { TextArea } from '@dhis2/ui'
+import Input from '@material-ui/core/Input'
 import ItemHeader from '../ItemHeader/ItemHeader'
+import PrintItemInfo from '../ItemHeader/PrintItemInfo'
 import Line from '../../../widgets/Line'
 import {
     Parser as RichTextParser,
@@ -18,9 +18,27 @@ import {
     sGetIsPrinting,
     sGetPrintDashboardItems,
 } from '../../../reducers/printDashboard'
-import { isEditMode } from '../../Dashboard/dashboardModes'
+import { isEditMode, PRINT_LAYOUT } from '../../Dashboard/dashboardModes'
 
-import classes from './styles/Item.module.css'
+const style = {
+    textDiv: {
+        padding: '10px',
+        whiteSpace: 'pre-line',
+        lineHeight: '20px',
+    },
+    textField: {
+        fontSize: '14px',
+        fontStretch: 'normal',
+        width: '90%',
+        margin: '0 auto',
+        display: 'block',
+        lineHeight: '24px',
+    },
+    container: {
+        marginBottom: '20px',
+        marginTop: '20px',
+    },
+}
 
 const TextItem = props => {
     const { item, dashboardMode, text, acUpdateDashboardItem } = props
@@ -28,42 +46,66 @@ const TextItem = props => {
     const onChangeText = event => {
         const updatedItem = {
             ...item,
-            text: event.value,
+            text: event.target.value,
         }
 
         acUpdateDashboardItem(updatedItem)
     }
 
-    const viewItem = () => (
-        <div className={cx(classes.viewContainer, 'dashboard-item-content')}>
-            <div className={classes.view}>
-                <RichTextParser>{text}</RichTextParser>
+    const viewItem = () => {
+        const textDivStyle = Object.assign({}, style.textField, style.textDiv)
+        return (
+            <div className="dashboard-item-content" style={style.container}>
+                <RichTextParser style={textDivStyle}>{text}</RichTextParser>
             </div>
-        </div>
-    )
+        )
+    }
 
-    const editItem = () => (
-        <>
-            <ItemHeader
-                title={i18n.t('Text item')}
-                itemId={item.id}
-                dashboardMode={dashboardMode}
-            />
-            <Line />
-            <div className="dashboard-item-content">
-                <RichTextEditor onEdit={onChangeText}>
-                    <TextArea
-                        className={classes.edit}
-                        value={text}
-                        placeholder={i18n.t('Add text here')}
-                        onChange={onChangeText}
-                    />
-                </RichTextEditor>
-            </div>
-        </>
-    )
+    const editItem = () => {
+        return (
+            <>
+                <ItemHeader
+                    title={i18n.t('Text item')}
+                    itemId={item.id}
+                    dashboardMode={dashboardMode}
+                />
+                <Line />
+                <div className="dashboard-item-content">
+                    <RichTextEditor onEdit={onChangeText}>
+                        <Input
+                            value={text}
+                            multiline
+                            fullWidth
+                            style={style.textField}
+                            placeholder={i18n.t('Add text here')}
+                            onChange={onChangeText}
+                        />
+                    </RichTextEditor>
+                </div>
+            </>
+        )
+    }
 
-    const textItem = isEditMode(dashboardMode) ? editItem : viewItem
+    const printItem = () => {
+        const textDivStyle = Object.assign({}, style.textField, style.textDiv)
+        return (
+            <>
+                {props.item.shortened ? <PrintItemInfo /> : null}
+                <div className="dashboard-item-content" style={style.container}>
+                    <RichTextParser style={textDivStyle}>{text}</RichTextParser>
+                </div>
+            </>
+        )
+    }
+
+    let textItem
+    if (isEditMode(dashboardMode)) {
+        textItem = editItem
+    } else if (dashboardMode === PRINT_LAYOUT) {
+        textItem = printItem
+    } else {
+        textItem = viewItem
+    }
 
     return <>{textItem()}</>
 }
