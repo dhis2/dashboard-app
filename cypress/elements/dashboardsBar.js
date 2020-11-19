bf2ffb0216dba6d4b34067a1c7726acce0c11e2f
Jen Jones Arnesen <jennifer@dhis2.org>
2020-11-19 21:55:18 +0100

    test: add timeout

--- cypress/elements/dashboardsBar.js
+++ cypress/elements/dashboardsBar.js
@@ -1,17 +1,19 @@
+const OPTIONS = { timeout: 10000 }
+
 const clickChip = name => {
-    cy.get('[data-test="dhis2-uicore-chip"]').contains(name).click()
+    cy.get('[data-test="dhis2-uicore-chip"]', OPTIONS).contains(name).click()
 }
 
 const toggleShowMoreButton = () => {
-    cy.get('[data-test="showmore-button"]').click()
+    cy.get('[data-test="showmore-button"]', OPTIONS).click()
 }
 
 const clickNew = () => {
-    cy.get('[data-test="link-new-dashboard"]').click()
+    cy.get('[data-test="link-new-dashboard"]', OPTIONS).click()
 }
 
 const clickItemSearch = () => {
-    cy.get('[data-test="item-search"]').click()
+    cy.get('[data-test="item-search"]', OPTIONS).click()
 }
 
 export const dashboardsBar = {
