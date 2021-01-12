f5c119d3fde575577499eeaabd6a5a03f713b6ed
Jen Jones Arnesen <jennifer@dhis2.org>
2021-01-12 08:13:50 +0100

    fix: cleanup

--- src/components/Dashboard/NotSupportedNotice.js
+++ src/components/Dashboard/NotSupportedNotice.js
@@ -5,17 +5,15 @@ import { NoticeBox, CenteredContent } from '@dhis2/ui'
 
 import classes from './styles/NotSupportedNotice.module.css'
 
-const NotSupportedNotice = ({ message }) => {
-    return (
-        <CenteredContent position="top">
-            <div className={classes.noticeContainer}>
-                <NoticeBox title={i18n.t('Not supported')} warning>
-                    {message}
-                </NoticeBox>
-            </div>
-        </CenteredContent>
-    )
-}
+const NotSupportedNotice = ({ message }) => (
+    <CenteredContent position="top">
+        <div className={classes.noticeContainer}>
+            <NoticeBox title={i18n.t('Not supported')} warning>
+                {message}
+            </NoticeBox>
+        </div>
+    </CenteredContent>
+)
 
 NotSupportedNotice.propTypes = {
     message: PropTypes.string,
