3e0b675f4dc738a697b2c36933e6cc6999b42198
Jen Jones Arnesen <jennifer@dhis2.org>
2021-01-21 16:45:29 +0100

    Revert "fix: hide tooltip also when the control bar is clicked to expand"
    This reverts commit 8b4b0b8d460a0df910848bcf7415a1c59664c2a6.

--- src/components/ControlBar/ShowMoreButton.js
+++ src/components/ControlBar/ShowMoreButton.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import i18n from '@dhis2/d2-i18n'
 import { Tooltip } from '@dhis2/ui'
@@ -9,46 +9,25 @@ import classes from './styles/ShowMoreButton.module.css'
 export const SHOWMORE_BAR_HEIGHT = 16
 
 const ShowMoreButton = ({ onClick, isMaxHeight, disabled }) => {
-    const containerRef = useRef(null)
     const buttonLabel = isMaxHeight
         ? i18n.t('Show fewer dashboards')
         : i18n.t('Show more dashboards')
-
-    const onButtonClicked = () => {
-        // The click may happen on the button svg or path
-        // element. This triggers the mouseout on the button
-        // element to ensure that the tooltip is removed
-        const buttonEl = containerRef.current.children[0]
-        const event = new MouseEvent('mouseout', {
-            bubbles: true,
-            cancelable: false,
-        })
-
-        onClick()
-        buttonEl.dispatchEvent(event)
-    }
-
     return (
-        <div className={classes.container} ref={containerRef}>
+        <div className={classes.container}>
             {disabled ? (
                 <div className={classes.disabled}>
                     <ChevronDown />
                 </div>
             ) : (
                 <Tooltip content={buttonLabel} placement="bottom">
-                    {({ onMouseOver, onMouseOut, ref }) => (
-                        <button
-                            className={classes.showMore}
-                            onClick={onButtonClicked}
-                            data-test="showmore-button"
-                            aria-label={buttonLabel}
-                            ref={ref}
-                            onMouseOver={onMouseOver}
-                            onMouseOut={onMouseOut}
-                        >
-                            {isMaxHeight ? <ChevronUp /> : <ChevronDown />}
-                        </button>
-                    )}
+                    <button
+                        className={classes.showMore}
+                        onClick={onClick}
+                        data-test="showmore-button"
+                        aria-label={buttonLabel}
+                    >
+                        {isMaxHeight ? <ChevronUp /> : <ChevronDown />}
+                    </button>
                 </Tooltip>
             )}
         </div>
