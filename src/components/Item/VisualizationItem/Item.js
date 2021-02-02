457c4402b89875dd69b54f44f43b268c87c0a5ba
Jen Jones Arnesen <jennifer@dhis2.org>
2021-02-02 14:56:05 +0100

    fix: restore some code to reduce PR scope

--- src/components/Item/VisualizationItem/Item.js
+++ src/components/Item/VisualizationItem/Item.js
@@ -62,14 +62,6 @@ export class Item extends Component {
     }
 
     async componentDidMount() {
-        const el = document.querySelector(this.itemDomElSelector)
-
-        if (el?.requestFullscreen) {
-            el.onfullscreenchange = this.handleFullscreenChange
-        } else if (el?.webkitRequestFullscreen) {
-            el.onwebkitfullscreenchange = this.handleFullscreenChange
-        }
-
         this.props.updateVisualization(
             await apiFetchVisualization(this.props.item)
         )
@@ -79,7 +71,7 @@ export class Item extends Component {
                 this.props.gatherDataStatistics &&
                 isViewMode(this.props.dashboardMode)
             ) {
-                apiPostDataStatistics(
+                await apiPostDataStatistics(
                     getDataStatisticsName(this.props.item.type),
                     getVisualizationId(this.props.item)
                 )
@@ -89,6 +81,14 @@ export class Item extends Component {
         }
 
         this.setState({ configLoaded: true })
+
+        const el = document.querySelector(this.itemDomElSelector)
+
+        if (el?.requestFullscreen) {
+            el.onfullscreenchange = this.handleFullscreenChange
+        } else if (el?.webkitRequestFullscreen) {
+            el.onwebkitfullscreenchange = this.handleFullscreenChange
+        }
     }
 
     componentWillUnmount() {
