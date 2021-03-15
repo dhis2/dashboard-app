2d5eb87a8b17816abbdf3c55c66ddc975b915569
Jen Jones Arnesen <jennifer@dhis2.org>
2021-03-15 14:11:37 +0100

    fix: test

--- cypress/integration/ui/edit_dashboard/sharing.js
+++ cypress/integration/ui/edit_dashboard/sharing.js
@@ -2,28 +2,26 @@ import { When, Then } from 'cypress-cucumber-preprocessor/steps'
 import { EXTENDED_TIMEOUT } from '../../../support/utils'
 import { dashboardTitleSel } from '../../../selectors/viewDashboard'
 
+const USER_NAME = 'Kevin Boateng'
+
 When('I change sharing settings', () => {
     cy.get('button').contains('Share', EXTENDED_TIMEOUT).click()
 
     //confirm that Boateng is not currently listed
-    cy.get('h6', EXTENDED_TIMEOUT)
-        .next('hr')
-        .next('div')
-        .children('div')
-        .should('have.length', 2)
+    cy.get('hr').should('have.length', 3)
 
     cy.get('[placeholder="Enter names"]').type('Boateng')
-    cy.contains('Kevin Boateng').click()
+    cy.contains(USER_NAME).click()
 
-    //Add him with View
-    cy.get('div').contains('Kevin Boateng').should('be.visible')
+    cy.get('div').contains(USER_NAME).should('be.visible')
 
     cy.get('button').contains('close', { matchCase: false }).click()
 })
+
 Then('the new sharing settings should be preserved', () => {
     cy.get(dashboardTitleSel, EXTENDED_TIMEOUT).should('be.visible')
     cy.get('button').contains('Share', EXTENDED_TIMEOUT).click()
-    cy.get('div')
-        .contains('Kevin Boateng', EXTENDED_TIMEOUT)
-        .should('be.visible')
+
+    cy.get('hr').should('have.length', 4)
+    cy.get('div').contains(USER_NAME, EXTENDED_TIMEOUT).should('be.visible')
 })
