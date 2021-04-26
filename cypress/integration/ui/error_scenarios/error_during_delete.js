0fd4d1c2f988fc3307ad308babe96f6382cd650b
Jen Jones Arnesen <jennifer@dhis2.org>
2021-04-26 10:41:16 +0200

    feat: add confirmation dialog for discarding changes to dashboard (#1713)
    This change was requested as part of the Dashboard PWA [DHIS2-10874]. (when editing a dashboard and connectivity lost, then the dialog can be used to warn user of lost changes if they exit).
    
    If a user makes any change (name, description, changes to items or filter settings), then the dirty state will be set and the dialog will be triggered when exiting without saving. However, adding translations does not affect the dirty status of the dashboard.

--- cypress/integration/ui/error_scenarios/error_during_delete.js
+++ cypress/integration/ui/error_scenarios/error_during_delete.js
@@ -1,7 +1,8 @@
 import { When } from 'cypress-cucumber-preprocessor/steps'
+import { confirmActionDialogSel } from '../../../selectors/editDashboard'
 
 When('A 500 error is thrown when I delete the dashboard', () => {
     cy.intercept('DELETE', '/dashboards', { statusCode: 500 })
     cy.get('button').contains('Delete').click()
-    cy.get('[data-test="confirm-delete-dashboard"]').click()
+    cy.get(confirmActionDialogSel).find('button').contains('Delete').click()
 })
