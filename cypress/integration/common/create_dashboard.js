f631710b638455f061009eefbb73c028777fd42e
Jen Jones Arnesen <jennifer@dhis2.org>
2021-05-05 15:11:30 +0200

    test: move things around

--- cypress/integration/common/create_dashboard.js
+++
@@ -1,5 +0,0 @@
-// import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
-
-// When('dashboard title is added', () => {
-//     cy.get('[data-test="dashboard-title-input"]').type(TEST_DASHBOARD_TITLE)
-// })
