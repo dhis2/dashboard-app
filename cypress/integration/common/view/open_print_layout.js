4f9e159a8320478d4b74bc406ca69a0a6e83d4fe
Jen Jones Arnesen <jennifer@dhis2.org>
2021-06-09 13:22:10 +0200

    fix: check item.type exists before using it

---
+++ cypress/integration/common/view/open_print_layout.js
@@ -0,0 +1,8 @@
+import { When } from 'cypress-cucumber-preprocessor/steps'
+import { clickViewActionButton } from '../../../elements/viewDashboard'
+
+When('I click to preview the print layout', () => {
+    clickViewActionButton('More')
+    cy.get('[data-test="print-menu-item"]').click()
+    cy.get('[data-test="print-layout-menu-item"]').click()
+})
