82c25efe81fcc0d62fbcf24f3bf87e4ed3871b60
Jen Jones Arnesen <jennifer@dhis2.org>
2021-08-26 11:06:19 +0200

    fix: remove unused fn

--- cypress/elements/editDashboard.js
+++ cypress/elements/editDashboard.js
@@ -8,12 +8,6 @@ export const itemSearchSel = '[data-test="item-search"]'
 
 export const actionsBarSel = '[data-test="edit-control-bar"]'
 
-export const getEditActionButton = action =>
-    cy
-        .get(actionsBarSel, EXTENDED_TIMEOUT)
-        .find('button')
-        .contains(action, EXTENDED_TIMEOUT)
-
 export const clickEditActionButton = action =>
     cy
         .get(actionsBarSel, EXTENDED_TIMEOUT)
