3c1fcb6b896eca5743fb03456094fdc96186b708
Jen Jones Arnesen <jennifer@dhis2.org>
2021-10-13 11:06:02 +0200

    fix: maps with ee layers should not load the ee layers when offline [v37] (#2032)
    Fixes https://jira.dhis2.org/browse/DHIS2-11908
    
    Earth Engine layers cannot be loaded while offline. If possible, the map will still be shown, but without the EE layers. A warning will be shown to the user

--- src/components/Item/VisualizationItem/Visualization/NoVisualizationMessage.js
+++ src/components/Item/VisualizationItem/Visualization/NoVisualizationMessage.js
@@ -1,9 +1,17 @@
+import { IconWarningFilled24, colors } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
 import classes from './styles/NoVisualizationMessage.module.css'
 
 const NoVisualizationMessage = ({ message }) => {
-    return <div className={classes.message}>{message}</div>
+    return (
+        <p className={classes.container}>
+            <span className={classes.icon}>
+                <IconWarningFilled24 color={colors.yellow700} />
+            </span>
+            <span className={classes.message}>{message}</span>
+        </p>
+    )
 }
 
 NoVisualizationMessage.propTypes = {
