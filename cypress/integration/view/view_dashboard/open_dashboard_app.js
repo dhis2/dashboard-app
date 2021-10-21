fdeab401b0a0e47c5da6f65ed97aaf00411c920e
Jen Jones Arnesen <jennifer@dhis2.org>
2021-10-21 14:06:24 +0200

    fix: username is needed to fetch preferred dashboard (#2045)
    Fixes: https://jira.dhis2.org/browse/DHIS2-12020
    
    Pass username to ViewDashboard, which needs it in redux connect to get the preferred dashboard id from local storage

---
+++ cypress/integration/view/view_dashboard/open_dashboard_app.js
@@ -0,0 +1,24 @@
+import { When, Then } from 'cypress-cucumber-preprocessor/steps'
+import {
+    dashboardTitleSel,
+    dashboardChipSel,
+} from '../../../elements/viewDashboard'
+import { EXTENDED_TIMEOUT } from '../../../support/utils'
+
+When('I open the dashboard app with the root url', () => {
+    cy.visit('/', EXTENDED_TIMEOUT)
+
+    cy.location().should(loc => {
+        expect(loc.hash).to.equal('#/')
+    })
+
+    cy.get(dashboardTitleSel).should('be.visible')
+    cy.get(dashboardChipSel, EXTENDED_TIMEOUT).should('be.visible')
+})
+
+Then('the {string} dashboard displays', title => {
+    cy.get(dashboardTitleSel).should('be.visible').and('contain', title)
+    cy.location().should(loc => {
+        expect(loc.hash).to.equal('#/')
+    })
+})
