baece25c0562969cadba3933db8f720711dda807
Jan Henrik Øverland <jan@dhis2.org>
2021-10-29 14:49:54 +0200

    feat: landing page (DHIS2-11989) (#2049)
    * first attempt
    
    * adjust icon pos
    
    * new page: start
    
    * move DashboardsBar to /components
    
    * DashboardBar state
    
    * close dashboard
    
    * online check
    
    * back to start screen link
    
    * clear state when closing dashboard
    
    * system setting + error handling
    
    * systemSettings variable
    
    * system settings mock
    
    * do not render most viewed when offline
    
    * rerender component when online changes
    
    * fix: remove console
    
    * fix: new snapshot for landing page
    
    * display available offline indicator
    
    * display available offline indicator
    
    * start screen outer height
    
    * start screen outer height
    
    * fix spacing and system setting key
    
    * remove data-test
    
    * update snapshots
    
    * revert lock
    
    Co-authored-by: Jen Jones Arnesen <jennifer@dhis2.org>

--- src/api/dataStatistics.js
+++ src/api/dataStatistics.js
@@ -1,5 +1,26 @@
 import { getInstance } from 'd2'
 
+export const apiGetDataStatistics = async (dataEngine, username) => {
+    const getDataStatisticsQuery = {
+        resource: 'dataStatistics/favorites',
+        params: {
+            eventType: 'DASHBOARD_VIEW',
+            pageSize: 6,
+            ...(username ? { username } : {}),
+        },
+    }
+
+    try {
+        const mostViewedDashboardsResult = await dataEngine.query({
+            dashboard: getDataStatisticsQuery,
+        })
+
+        return mostViewedDashboardsResult
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+}
+
 export const apiPostDataStatistics = async (eventType, id) => {
     const d2 = await getInstance()
     const url = `dataStatistics?eventType=${eventType}&favorite=${id}`
