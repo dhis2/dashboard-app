424a0a1704aa4cc73bb9c3ddb1814b2140445d0d
Jen Jones Arnesen <jennifer@dhis2.org>
2022-05-30 14:47:09 +0200

    fix: App wrapper test warnings

--- src/__tests__/AppWrapper.spec.js
+++ src/__tests__/AppWrapper.spec.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppWrapper from '../AppWrapper.js'
 
+jest.mock('@dhis2/app-runtime-adapter-d2', () => {
+    return {
+        D2Shim: ({ children }) => children({ d2: {} }),
+    }
+})
+
 jest.mock(
     '../components/App',
     () =>
@@ -9,13 +15,23 @@ jest.mock(
             return <div />
         }
 )
-jest.mock('@dhis2/app-runtime-adapter-d2', () => {
-    return {
-        D2Shim: ({ children }) => children({ d2: {} }),
-    }
-})
 
-it('renders without crashing', () => {
+jest.mock(
+    '../components/SystemSettingsProvider.js',
+    () =>
+        function Mock() {
+            return <div className="SystemSettingsProvider" />
+        }
+)
+jest.mock(
+    '../components/UserSettingsProvider.js',
+    () =>
+        function Mock() {
+            return <div className="UserSettingsProvider" />
+        }
+)
+
+test('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<AppWrapper />, div)
     ReactDOM.unmountComponentAtNode(div)
