2195c637ffff2647950bf09d9c40d4350769ae09
Edoardo Sabadelli <edoardo@dhis2.org>
2022-09-08 11:42:12 +0200

    feat: allow iframe plugin overrides for dev/test
    (cherry picked from commit f79f622539eb88611a5ac86659eb40d345a28837)

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -4,3 +4,6 @@ export const getPreferredDashboardId = username =>
 export const storePreferredDashboardId = (username, dashboardId) => {
     localStorage.setItem(`dhis2.dashboard.current.${username}`, dashboardId)
 }
+
+export const getPluginOverrides = () =>
+    process.env.NODE_ENV !== 'production' && JSON.parse(localStorage.getItem('dhis2.dashboard.pluginOverrides')) || undefined
