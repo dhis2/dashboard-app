dc16ab14769b17f0fac0be7046225464297e23dc
Edoardo Sabadelli <edoardo@dhis2.org>
2022-09-14 16:15:05 +0200

    fix: fix tests

--- src/AppWrapper.spec.js
+++ src/AppWrapper.spec.js
@@ -15,6 +15,21 @@ jest.mock('@dhis2/app-runtime-adapter-d2', () => {
     }
 })
 
+jest.mock(
+    './components/SystemSettingsProvider.js',
+    () =>
+        function Mock() {
+            return <div className="SystemSettingsProvider" />
+        }
+)
+jest.mock(
+    './components/UserSettingsProvider.js',
+    () =>
+        function Mock() {
+            return <div className="UserSettingsProvider" />
+        }
+)
+
 it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<AppWrapper />, div)
