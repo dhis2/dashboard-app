0882d2d124f73e236c73a1ee4b84c392384b4636
Edoardo Sabadelli <edoardo@dhis2.org>
2022-09-20 11:33:04 +0200

    fix: remove Suspense which might interfere with the iframe
    (cherry picked from commit d33336bf04a23cd8f07e0b315262cb06308163f8)

--- src/components/Item/VisualizationItem/Visualization/IframePlugin.js
+++ src/components/Item/VisualizationItem/Visualization/IframePlugin.js
@@ -73,15 +73,13 @@ const IframePlugin = ({
 
     // TODO use different URL for iframe src based on item.type
     return (
-        <Suspense fallback={<div />}>
-            <div className={classes.wrapper}>
-                <iframe
-                    ref={iframeRef}
-                    src={getIframeSrc()}
-                    style={{ width: '100%', height: '100%', border: 'none' }}
-                ></iframe>
-            </div>
-        </Suspense>
+        <div className={classes.wrapper}>
+            <iframe
+                ref={iframeRef}
+                src={getIframeSrc()}
+                style={{ width: '100%', height: '100%', border: 'none' }}
+            ></iframe>
+        </div>
     )
 }
 
