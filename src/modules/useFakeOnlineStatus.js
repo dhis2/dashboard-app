c3b78ef39e45e31d91fbc1328dded76353bdb7eb
Kai Vandivier <kpvandivier@gmail.com>
2022-10-30 09:55:36 +0100

    refactor: useFakeOnlineStatus

--- src/modules/useFakeOnlineStatus.js
+++ src/modules/useFakeOnlineStatus.js
@@ -1,24 +1,17 @@
 import { useOnlineStatus as useOriginalOnlineStatus } from '@dhis2/app-runtime'
 
-console.log({ env: process.env.NODE_ENV })
-
 /**
  * This is intended for a hot-fix type release, where this just returns
  * `online: true` so that offline local deployments get unblocked.
  * See https://dhis2.atlassian.net/browse/DHIS2-12937
  *
- * In test environments, use the existing hook to avoid remaking them
+ * In the future, this should be fixed by using a smarter connection
+ * status-detecting hook; see https://dhis2.atlassian.net/browse/LIBS-315
  */
-export const useOnlineStatus = (...args) => {
-    const originalHook = useOriginalOnlineStatus(...args)
+const useFakeOnlineStatus = () => ({ online: true, offline: false })
 
-    if (process.env.NODE_ENV === 'test') {
-        return originalHook
-    }
-
-    return {
-        online: true,
-        offline: false,
-        lastOnline: undefined,
-    }
-}
+/** In test environments, use the original hook to avoid breaking tests */
+export const useOnlineStatus =
+    process.env.NODE_ENV === 'test'
+        ? useOriginalOnlineStatus
+        : useFakeOnlineStatus
