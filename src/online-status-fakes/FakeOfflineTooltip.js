c4a882c219ae95b2ae3a31232b1afa39271c9492
Kai Vandivier <kpvandivier@gmail.com>
2022-10-30 10:02:45 +0100

    refactor: fake offline tooltip

--- src/online-status-fakes/FakeOfflineTooltip.js
+++ src/online-status-fakes/FakeOfflineTooltip.js
@@ -1,10 +1,14 @@
 import { OfflineTooltip as OriginalOfflineTooltip } from '@dhis2/analytics'
 
+/**
+ * This is part of the hotfix described in useFakeOnlineStatus.js
+ *
+ * The original offline tooltip consumes useOnlineStatus too, so we mock this
+ * with a fake (except for in tests, so they don't break in this branch)
+ */
 const FakeOfflineTooltip = ({ children }) => children
 
-const getOfflineTooltipComponent = () =>
+export const OfflineTooltip =
     process.env.NODE_ENV === 'test'
         ? OriginalOfflineTooltip
         : FakeOfflineTooltip
-
-export const OfflineTooltip = getOfflineTooltipComponent()
