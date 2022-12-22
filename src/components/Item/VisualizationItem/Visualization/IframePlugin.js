8b738a9cbc5e0f46b29bd248c2ba340bf10dd8c6
Edoardo Sabadelli <edoardo@dhis2.org>
2022-12-22 17:12:35 +0100

    refactor: attempt at using MessageChannel in place of postRobot
    The solution is a bit more convoluted and require some boilerplate code
    on both sides of the communication channel.

--- src/components/Item/VisualizationItem/Visualization/IframePlugin.js
+++ src/components/Item/VisualizationItem/Visualization/IframePlugin.js
@@ -1,7 +1,6 @@
 import { useD2 } from '@dhis2/app-runtime-adapter-d2'
-import postRobot from '@krakenjs/post-robot'
 import PropTypes from 'prop-types'
-import React, { useRef, useCallback, useState, useEffect } from 'react'
+import React, { useRef, useCallback, useMemo, useState, useEffect } from 'react'
 import {
     CHART,
     REPORT_TABLE,
@@ -29,6 +28,7 @@ const IframePlugin = ({
     const { userSettings } = useUserSettings()
     const iframeRef = useRef()
     const [error, setError] = useState(null)
+    const channelRef = useRef(new MessageChannel())
 
     // When this mounts, check if the dashboard is recording
     const { isCached, recordingState } = useCacheableSection(dashboardId)
@@ -36,68 +36,106 @@ const IframePlugin = ({
         recordingState === 'recording'
     )
 
-    const onError = () => setError('plugin')
+    const pluginProps = useMemo(
+        () => ({
+            isVisualizationLoaded: true,
+            forDashboard: true,
+            displayProperty: userSettings.displayProperty,
+            visualization,
+            onError: () => setError('plugin'),
 
-    const pluginProps = {
-        isVisualizationLoaded: true,
-        forDashboard: true,
-        displayProperty: userSettings.displayProperty,
-        visualization,
-        onError,
+            // For caching: ---
+            // Add user & dashboard IDs to cache ID to avoid removing a cached
+            // plugin that might be used in another dashboard also
+            // TODO: May also want user ID too for multi-user situations
+            cacheId: `${dashboardId}-${item.id}`,
+            isParentCached: isCached,
+            recordOnNextLoad: recordOnNextLoad,
+        }),
+        [
+            userSettings.displayProperty,
+            visualization,
+            dashboardId,
+            isCached,
+            item.id,
+            recordOnNextLoad,
+        ]
+    )
 
-        // For caching: ---
-        // Add user & dashboard IDs to cache ID to avoid removing a cached
-        // plugin that might be used in another dashboard also
-        // TODO: May also want user ID too for multi-user situations
-        cacheId: `${dashboardId}-${item.id}`,
-        isParentCached: isCached,
-        recordOnNextLoad: recordOnNextLoad,
-    }
+    useEffect(() => {
+        const onMessage = (e) => {
+            console.log('message received in app', e)
+
+            // cannot test e.origin
+            if (e.data === 'requestPort') {
+                channelRef.current = new MessageChannel()
+                channelRef.current.port1.onmessage = (e) => {
+                    console.log('message received in app via channel', e)
+                    let data
+
+                    try {
+                        data = JSON.parse(e.data)
+                    } catch (err) {
+                        console.log(
+                            'Event data is not a serialised JSON object'
+                        )
+                    }
+
+                    // TODO not really required here since it's only messages in the private channel
+                    if (data.type === 'DHIS2') {
+                        switch (data.command) {
+                            case 'requestProps': {
+                                if (recordOnNextLoad) {
+                                    // Avoid recording unnecessarily,
+                                    // e.g. if plugin re-requests props for some reason
+                                    setRecordOnNextLoad(false)
+                                }
+                                console.log('send props')
+                                channelRef.current.port1.postMessage(
+                                    JSON.stringify({
+                                        type: 'DHIS2',
+                                        command: 'sendProps',
+                                        payload: pluginProps,
+                                    })
+                                )
+                                break
+                            }
+                        }
+                    }
+                }
+
+                iframeRef.current.contentWindow.postMessage(
+                    'sendPort',
+                    e.origin,
+                    [channelRef.current.port2]
+                )
+            }
+        }
+
+        window.addEventListener('message', onMessage)
+
+        return () => window.removeEventListener('message', onMessage)
+    }, [pluginProps, recordOnNextLoad])
 
     useEffect(() => {
         // Tell plugin to remove cached data if this dashboard has been removed
         // from offline storage
         if (iframeRef?.current && !isCached) {
-            postRobot
-                .send(iframeRef.current.contentWindow, 'removeCachedData')
-                .catch((err) => {
-                    // catch error if iframe hasn't loaded yet
-                    const msg = 'No handler found for post message:'
-                    if (err.message.startsWith(msg)) {
-                        return
-                    }
-                    console.error(err)
-                })
+            channelRef.current.port1.postMessage(
+                JSON.stringify({ type: 'DHIS2', command: 'removeCachedData' })
+            )
         }
     }, [isCached])
 
     useEffect(() => {
-        if (iframeRef?.current) {
-            const listener = postRobot.on(
-                'getProps',
-                // listen for messages coming only from the iframe rendered by this component
-                { window: iframeRef.current.contentWindow },
-                () => {
-                    if (recordOnNextLoad) {
-                        // Avoid recording unnecessarily,
-                        // e.g. if plugin re-requests props for some reason
-                        setRecordOnNextLoad(false)
-                    }
-
-                    return pluginProps
-                }
-            )
-
-            return () => listener.cancel()
-        }
-    }, [pluginProps, recordOnNextLoad])
-
-    useEffect(() => {
-        if (iframeRef.current?.contentWindow) {
-            postRobot.send(
-                iframeRef.current.contentWindow,
-                'newProps',
-                pluginProps
+        console.log('send props on dep change')
+        if (channelRef.current?.port1) {
+            channelRef.current.port1.postMessage(
+                JSON.stringify({
+                    type: 'DHIS2',
+                    command: 'sendProps',
+                    payload: pluginProps,
+                })
             )
         }
     }, [pluginProps])
