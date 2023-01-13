a85c22642a19610fbeb3cd3fd30329d7802532dd
Edoardo Sabadelli <edoardo@dhis2.org>
2023-01-13 10:33:13 +0100

    fix: use new endpoint for dashboard item search DHIS2-12236 (#2204)
    * fix: use new endpoint for dashboard item search DHIS2-12236
    
    * test: use more sensible viewport for tests
    
    This is the same size we use in DV/LL.
    
    * test: use a different map to avoid timeouts
    
    This map seems to be loading fast enough.
    The super long timeout has been removed.

--- src/pages/edit/ItemSelector/dashboardsQQuery.js
+++
@@ -1,6 +0,0 @@
-export const getDashboardsQQuery = (query = '', maxItems = []) => {
-    return {
-        resource: `dashboards/q/${query}`,
-        params: { count: 11, max: maxItems },
-    }
-}
