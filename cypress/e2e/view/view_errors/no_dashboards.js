ae19968963265f994ec16eb57466761c8cf0b497
Edoardo Sabadelli <edoardo@dhis2.org>
2023-03-29 12:59:25 +0200

    fix: upgrade to Cypress 12

---
+++ cypress/e2e/view/view_errors/no_dashboards.js
@@ -0,0 +1,14 @@
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor'
+import { newButtonSel } from '../../../elements/viewDashboard.js'
+import { EXTENDED_TIMEOUT } from '../../../support/utils.js'
+
+// Scenario: There are no dashboards
+Given('I open an app with no dashboards', () => {
+    cy.intercept('/dashboards', { dashboards: [] })
+    cy.visit('/', EXTENDED_TIMEOUT)
+})
+
+Then('a message displays informing that there are no dashboards', () => {
+    cy.contains('No dashboards found', EXTENDED_TIMEOUT).should('be.visible')
+    cy.get(newButtonSel).should('be.visible')
+})
