e34c67ab6b3c06c7859c80b3276ab5b2b57603ca
Edoardo Sabadelli <edoardo@dhis2.org>
2023-04-12 16:04:40 +0200

    fix: add custom command for handling iframe content

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,12 @@
-import { enableAutoLogin, enableNetworkShim, isStubMode, networkModes } from '@dhis2/cypress-commands'
+import {
+    enableAutoLogin,
+    enableNetworkShim,
+    isStubMode,
+    networkModes,
+} from '@dhis2/cypress-commands'
 
 const getDefaultMode = () => {
-  return networkModes.STUB
+    return networkModes.STUB
 }
 
 enableNetworkShim()
@@ -19,3 +24,48 @@ Cypress.on('uncaught:exception', (err) => {
         return false
     }
 })
+
+// Define a Cypress command to get the body of an iframe
+const sleep = (timeout) =>
+    new Promise((resolve) => setTimeout(resolve, timeout))
+
+Cypress.Commands.add(
+    'getIframeBody',
+    { prevSubject: 'element' },
+    (subject, iframeSelector = 'iframe') => {
+        cy.wrap(subject)
+            .find(iframeSelector)
+            .then({ timeout: 30000 }, async ($iframe) => {
+                if ($iframe.length !== 1) {
+                    console.error('more than 1 iframe', $iframe.length)
+                    throw new Error(
+                        `getIframeBody command can only be applied to one iframe at a time. Instead found ${$iframe.length}`
+                    )
+                }
+
+                const contentWindow = $iframe.prop('contentWindow')
+
+                while (contentWindow.location.toString() === 'about:blank') {
+                    console.log('wait for iframe location')
+                    await sleep(100)
+                }
+
+                if (contentWindow.document.readyState === 'complete') {
+                    console.log('return body', contentWindow.document.body)
+                    return contentWindow.document.body
+                }
+
+                await new Promise((resolve) => {
+                    Cypress.$(contentWindow).on('load', resolve)
+                })
+
+                console.log(
+                    'return body after promise',
+                    contentWindow.document.body
+                )
+
+                return contentWindow.document.body
+            })
+            .then(cy.wrap)
+    }
+)
