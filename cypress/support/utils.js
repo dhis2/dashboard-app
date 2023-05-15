4cc979ca56af4ea8a350f983fa1236ae8e8a457e
Jen Jones Arnesen <jennifer@dhis2.org>
2023-05-15 10:45:59 +0200

    fix: upgrade to cypress-commands 10

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,11 +1,11 @@
 export const EXTENDED_TIMEOUT = { timeout: 45000 }
 
 export const getApiBaseUrl = () => {
-    const baseUrl = Cypress.env('dhis2_base_url') || ''
+    const baseUrl = Cypress.env('dhis2BaseUrl') || ''
 
     if (!baseUrl) {
         throw new Error(
-            'No `dhis2_base_url` found. Please make sure to add it to `cypress.env.json`'
+            'No `dhis2BaseUrl` found. Please make sure to add it to `cypress.env.json`'
         )
     }
 
