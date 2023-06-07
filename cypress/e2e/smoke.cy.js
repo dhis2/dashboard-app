103c02d21a02c31c79f3d74363343fd105d04885
Jen Jones Arnesen <jennifer@dhis2.org>
2023-06-07 15:29:01 +0200

    fix: extended timeout

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -3,7 +3,9 @@ import { EXTENDED_TIMEOUT } from '../support/utils.js'
 describe('Smoke test', () => {
     it('opens the app', () => {
         cy.visit('/', EXTENDED_TIMEOUT).log(Cypress.env('dhis2BaseUrl'))
-        cy.getBySel('dashboard-chip').contains('Delivery').click()
+        cy.getBySel('dashboard-chip', EXTENDED_TIMEOUT)
+            .contains('Delivery')
+            .click()
 
         cy.getBySel('view-dashboard-title')
             .contains('Delivery')
