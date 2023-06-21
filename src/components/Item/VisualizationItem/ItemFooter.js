0e57cc2c9643e4880f95ab70ea9667456543f239
Jen Jones Arnesen <jennifer@dhis2.org>
2023-06-21 10:52:28 +0200

    fix: use analytics interpretations component

--- src/components/Item/VisualizationItem/ItemFooter.js
+++ src/components/Item/VisualizationItem/ItemFooter.js
@@ -1,18 +1,22 @@
-import { AboutAOUnit, InterpretationsUnit } from '@dhis2/analytics'
+import { AboutAOUnit, InterpretationsWrapper } from '@dhis2/analytics'
+import { useConfig } from '@dhis2/app-runtime'
 import { useD2 } from '@dhis2/app-runtime-adapter-d2'
 import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import { getVisualizationId } from '../../../modules/item.js'
+import { getItemUrl } from '../../../modules/itemTypes.js'
 import FatalErrorBoundary from './FatalErrorBoundary.js'
 import classes from './styles/ItemFooter.module.css'
 
 const ItemFooter = (props) => {
+    const { baseUrl } = useConfig()
+    const [interpretationId, setInterpretationId] = useState(null)
     const { d2 } = useD2()
-    const id = getVisualizationId(props.item)
 
-    console.log('currentuser', d2.currentUser)
-    console.log('type', props.item.type.toLowerCase())
+    const id = getVisualizationId(props.item)
+    const launchUrl = getItemUrl(props.item.type, { id }, baseUrl)
+
     return (
         <div className={classes.itemFooter} data-test="dashboarditem-footer">
             <hr className={classes.line} />
@@ -23,18 +27,16 @@ const ItemFooter = (props) => {
                     )}
                 >
                     <AboutAOUnit type="visualization" id={id} />
-                    <InterpretationsUnit
-                        // ref={interpretationsUnitRef}
+                    <InterpretationsWrapper
                         type={props.item.type.toLowerCase()}
                         id={id}
+                        interpretationId={interpretationId}
                         currentUser={d2.currentUser}
-                        // onInterpretationClick={(interpretationId) =>
-                        //     navigateToOpenModal(interpretationId)
-                        // }
-                        // onReplyIconClick={(interpretationId) =>
-                        //     navigateToOpenModal(interpretationId, true)
-                        // }
-                        // disabled={disabled}
+                        onReplyIconClick={setInterpretationId}
+                        onGoBackClicked={() => setInterpretationId(null)}
+                        onInterpretationClick={Function.prototype}
+                        launchUrl={launchUrl}
+                        inlineReply={true}
                     />
                 </FatalErrorBoundary>
             </div>
