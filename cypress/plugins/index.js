d9ee370ff8d8c577efa588df87aa108383d83678
Adel Bensaad <adel@dhis2.org>
2023-07-19 10:48:01 +0100

    test: publish cypress test results to reportportal

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,8 @@
 const plugins = require('@dhis2/cli-utils-cypress/plugins')
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin')
 const getCypressEnvVars = require('./getCypressEnvVars.js')
+
+module.exports = (on, config) => registerReportPortalPlugin(on, config)
 
 module.exports = (on, config) => {
     on('before:browser:launch', (browser, launchOptions) => {
@@ -20,6 +23,7 @@ module.exports = (on, config) => {
     plugins(on, config)
 
     config.env = getCypressEnvVars(config)
+    config.reporterOptions.apiKey = process.env.CYPRESS_RP_API_KEY
 
     return config
 }
