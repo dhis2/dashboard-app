42be6df28fe71cb22602f8a5b532ebefcecda213
Jen Jones Arnesen <jennifer@dhis2.org>
2023-09-05 07:44:00 +0200

    fix: test cleanup

--- src/components/Item/AppItem/getIframeSrc.js
+++ src/components/Item/AppItem/getIframeSrc.js
@@ -3,8 +3,6 @@ import { FILTER_ORG_UNIT } from '../../../actions/itemFilters.js'
 export const getIframeSrc = (appDetails, item, itemFilters) => {
     let iframeSrc = `${appDetails.launchUrl}?dashboardItemId=${item.id}`
 
-    console.log('jj itemfilters', itemFilters)
-
     if (itemFilters[FILTER_ORG_UNIT] && itemFilters[FILTER_ORG_UNIT].length) {
         const ouIds = itemFilters[FILTER_ORG_UNIT].map(({ id, path }) =>
             path ? path.split('/').slice(-1)[0] : id
