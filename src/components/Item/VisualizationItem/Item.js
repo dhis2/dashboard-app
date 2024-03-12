7b207794bca9f86d0895026f6a46c726d52d65d7
Edoardo Sabadelli <edoardo@dhis2.org>
2024-03-12 10:33:43 +0100

    fix: avoid re-rendering an item when adding the same again (DHIS2-17016)
    This also avoids an unnecessary fetch of the same visualization object.
    
    (cherry picked from commit d4b3f4ddca037bedff9c1f459fd0fcddb7624cc8)

--- src/components/Item/VisualizationItem/Item.js
+++ src/components/Item/VisualizationItem/Item.js
@@ -81,9 +81,13 @@ class Item extends Component {
     }
 
     async componentDidMount() {
-        this.props.setVisualization(
-            await apiFetchVisualization(this.props.item)
-        )
+        // Avoid refetching the visualization already in the Redux store
+        // when the same dashboard item is added again.
+        // This also solves a flashing of all the "duplicated" dashboard items.
+        !this.props.visualization.id &&
+            this.props.setVisualization(
+                await apiFetchVisualization(this.props.item)
+            )
 
         try {
             if (
