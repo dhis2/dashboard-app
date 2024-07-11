50771b2277ed19b8769dab4c8c41c9b10e641daf
Edoardo Sabadelli <edoardo@dhis2.org>
2024-07-11 09:58:47 +0200

    fix: move visualization fetch in try/catch
    This is to avoid the error boundary to show up for uncaught errors
    (failed to fetch a visualization) which
    interferes with Cypress testing.

--- src/components/Item/VisualizationItem/Item.js
+++ src/components/Item/VisualizationItem/Item.js
@@ -95,15 +95,15 @@ class Item extends Component {
     }
 
     async componentDidMount() {
-        // Avoid refetching the visualization already in the Redux store
-        // when the same dashboard item is added again.
-        // This also solves a flashing of all the "duplicated" dashboard items.
-        !this.props.visualization.id &&
-            this.props.setVisualization(
-                await apiFetchVisualization(this.props.item)
-            )
-
         try {
+            // Avoid refetching the visualization already in the Redux store
+            // when the same dashboard item is added again.
+            // This also solves a flashing of all the "duplicated" dashboard items.
+            !this.props.visualization.id &&
+                this.props.setVisualization(
+                    await apiFetchVisualization(this.props.item)
+                )
+
             if (
                 this.props.settings
                     .keyGatherAnalyticalObjectStatisticsInDashboardViews &&
