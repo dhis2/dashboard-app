a911ab41f50bd0362b2fbdc1bb9a8277c1684230
Edoardo Sabadelli <edoardo@dhis2.org>
2024-08-09 15:32:27 +0200

    feat: support custom dashboard plugins (DHIS2-17283)

--- src/components/Item/AppItem/Item.js
+++ src/components/Item/AppItem/Item.js
@@ -1,19 +1,27 @@
+// eslint-disable-next-line import/no-unresolved
+import { Plugin } from '@dhis2/app-runtime/experimental'
 import i18n from '@dhis2/d2-i18n'
 import { Divider, colors, spacers, IconQuestion24 } from '@dhis2/ui'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import { EDIT, isEditMode } from '../../../modules/dashboardModes.js'
+import { useCacheableSection } from '../../../modules/useCacheableSection.js'
 import {
     sGetItemFiltersRoot,
     DEFAULT_STATE_ITEM_FILTERS,
 } from '../../../reducers/itemFilters.js'
+import { sGetSelectedId } from '../../../reducers/selected.js'
 import ItemHeader from '../ItemHeader/ItemHeader.js'
 import { getIframeSrc } from './getIframeSrc.js'
 import styles from './styles/AppItem.module.css'
 
 const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
+    const dashboardId = useSelector(sGetSelectedId)
+
+    const { isCached } = useCacheableSection(dashboardId)
+
     let appDetails
 
     const appKey = item.appKey
@@ -26,7 +34,9 @@ const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
         appDetails?.settings?.dashboardWidget?.hideTitle &&
         dashboardMode !== EDIT
 
-    return appDetails && appDetails.name && appDetails.launchUrl ? (
+    const iframeSrc = getIframeSrc(appDetails, item, itemFilters)
+
+    return iframeSrc ? (
         <>
             {!hideTitle && (
                 <>
@@ -39,15 +49,27 @@ const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
                     <Divider margin={`0 0 ${spacers.dp4} 0`} />
                 </>
             )}
-            <iframe
-                title={appDetails.name}
-                src={getIframeSrc(appDetails, item, itemFilters)}
-                className={cx(styles.content, {
+
+            {appDetails.appType === 'APP' ? (
+                // new plugins
+                <Plugin
+                    pluginSource={iframeSrc}
+                    dashboardItemId={item.id}
+                    cacheId={`${dashboardId}-${item.id}`}
+                    isParentCached={isCached}
+                />
+            ) : (
+                // legacy widgets
+                <iframe
+                    title={appDetails.name}
+                    src={iframeSrc}
+                    className={cx(styles.content, {
                     [styles.hiddenTitle]: hideTitle,
                     [styles.fullscreen]: isFullscreen,
                 })}
-                style={{ border: 'none' }}
-            />
+                    style={{ border: 'none' }}
+                />
+            )}
         </>
     ) : (
         <>
