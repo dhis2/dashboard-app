9d40c0287afa64111dbbeeefd0cb442f36894bbe
Edoardo Sabadelli <edoardo@dhis2.org>
2024-08-09 15:32:27 +0200

    feat: support custom dashboard plugins (DHIS2-17283)

--- src/components/Item/AppItem/Item.js
+++ src/components/Item/AppItem/Item.js
@@ -1,18 +1,25 @@
 import { useD2 } from '@dhis2/app-runtime-adapter-d2'
+// eslint-disable-next-line import/no-unresolved
+import { Plugin } from '@dhis2/app-runtime/experimental'
 import { Divider, colors, spacers, IconQuestion24 } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import { EDIT, isEditMode } from '../../../modules/dashboardModes.js'
+import { useCacheableSection } from '../../../modules/useCacheableSection.js'
 import {
     sGetItemFiltersRoot,
     DEFAULT_STATE_ITEM_FILTERS,
 } from '../../../reducers/itemFilters.js'
+import { sGetSelectedId } from '../../../reducers/selected.js'
 import ItemHeader from '../ItemHeader/ItemHeader.js'
 import { getIframeSrc } from './getIframeSrc.js'
 
 const AppItem = ({ dashboardMode, item, itemFilters }) => {
     const { d2 } = useD2()
+    const dashboardId = useSelector(sGetSelectedId)
+
+    const { isCached } = useCacheableSection(dashboardId)
 
     let appDetails
 
@@ -26,7 +33,9 @@ const AppItem = ({ dashboardMode, item, itemFilters }) => {
         appDetails?.settings?.dashboardWidget?.hideTitle &&
         dashboardMode !== EDIT
 
-    return appDetails && appDetails.name && appDetails.launchUrl ? (
+    const iframeSrc = getIframeSrc(appDetails, item, itemFilters)
+
+    return iframeSrc ? (
         <>
             {!hideTitle && (
                 <>
@@ -39,16 +48,27 @@ const AppItem = ({ dashboardMode, item, itemFilters }) => {
                     <Divider margin={`0 0 ${spacers.dp4} 0`} />
                 </>
             )}
-            <iframe
-                title={appDetails.name}
-                src={getIframeSrc(appDetails, item, itemFilters)}
-                className={
-                    !hideTitle
-                        ? 'dashboard-item-content'
-                        : 'dashboard-item-content-hidden-title'
-                }
-                style={{ border: 'none' }}
-            />
+            {appDetails.appType === 'APP' ? (
+                // new plugins
+                <Plugin
+                    pluginSource={iframeSrc}
+                    dashboardItemId={item.id}
+                    cacheId={`${dashboardId}-${item.id}`}
+                    isParentCached={isCached}
+                />
+            ) : (
+                // legacy widgets
+                <iframe
+                    title={appDetails.name}
+                    src={iframeSrc}
+                    className={
+                        !hideTitle
+                            ? 'dashboard-item-content'
+                            : 'dashboard-item-content-hidden-title'
+                    }
+                    style={{ border: 'none' }}
+                />
+            )}
         </>
     ) : (
         <>
