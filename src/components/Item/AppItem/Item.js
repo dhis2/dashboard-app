df41ff6c704073684e66196bc93f55855c71ffb3
Edoardo Sabadelli <edoardo@dhis2.org>
2024-08-30 15:28:06 +0200

    refactor: pass width/height to AppItem to fix resize/refresh issue (DHIS2-17947)
    The same solution is used in VisualizationItem

--- src/components/Item/AppItem/Item.js
+++ src/components/Item/AppItem/Item.js
@@ -3,7 +3,7 @@ import { Plugin } from '@dhis2/app-runtime/experimental'
 import { Divider, spacers } from '@dhis2/ui'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useMemo, useRef } from 'react'
 import { connect, useSelector } from 'react-redux'
 import { EDIT, isEditMode } from '../../../modules/dashboardModes.js'
 import { useCacheableSection } from '../../../modules/useCacheableSection.js'
@@ -12,12 +12,15 @@ import {
     DEFAULT_STATE_ITEM_FILTERS,
 } from '../../../reducers/itemFilters.js'
 import { sGetSelectedId } from '../../../reducers/selected.js'
+import { getAvailableDimensions } from '../getAvailableDimensions.js'
 import ItemHeader from '../ItemHeader/ItemHeader.js'
 import MissingPluginMessage from '../ItemMessage/MissingPluginMessage.js'
 import { getIframeSrc } from './getIframeSrc.js'
 import styles from './styles/AppItem.module.css'
 
-const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
+const AppItem = ({ dashboardMode, windowDimensions, item, itemFilters, apps, isFullscreen }) => {
+    const contentRef = useRef()
+    const headerRef = useRef()
     const dashboardId = useSelector(sGetSelectedId)
 
     const { isCached } = useCacheableSection(dashboardId)
@@ -30,9 +33,62 @@ const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
         appDetails = apps.find((app) => app.key === appKey)
     }
 
+    const pluginProps = useMemo(
+        () => ({
+            dashboardItemId: item.id,
+            dashboardItemFilters: itemFilters,
+            cacheId: `${dashboardId}-${item.id}`,
+            isParentCached: isCached,
+        }),
+        [dashboardId, item.id, isCached, itemFilters]
+    )
+
+    // See DHIS2-9605
     const hideTitle =
         appDetails?.settings?.dashboardWidget?.hideTitle &&
         dashboardMode !== EDIT
+
+    const renderPlugin = (iframeSrc) => {
+        // style must be computed at runtime.
+        // header/content elements need to be rendered first, otherwise the dimensions returned are the previous ones
+        const style =
+            headerRef.current && contentRef.current
+                ? getAvailableDimensions({
+                      item,
+                      headerRef,
+                      contentRef,
+                      dashboardMode,
+                      windowDimensions,
+                      isFullscreen,
+                  })
+                : {}
+
+        return appDetails.appType === 'APP' ? (
+            // modern plugins
+            <Plugin
+                pluginSource={iframeSrc}
+                hasFixedDimensions={true}
+                width={style.width}
+                height={style.height}
+                {...pluginProps}
+            />
+        ) : (
+            // legacy widgets
+            <iframe
+                title={appDetails.name}
+                src={iframeSrc}
+                className={cx(styles.content, {
+                            [styles.hiddenTitle]: hideTitle,
+                            [styles.fullscreen]: isFullscreen,
+                        })}
+                style={{
+                    border: 'none',
+                    width: style.width,
+                    height: style.height,
+                }}
+            />
+        )
+    }
 
     if (appDetails) {
         const iframeSrc = getIframeSrc(appDetails, item, itemFilters)
@@ -42,6 +98,7 @@ const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
                 {!hideTitle && (
                     <>
                         <ItemHeader
+                            ref={headerRef}
                             title={appDetails.name}
                             itemId={item.id}
                             dashboardMode={dashboardMode}
@@ -50,27 +107,9 @@ const AppItem = ({ dashboardMode, item, itemFilters, apps, isFullscreen }) => {
                         <Divider margin={`0 0 ${spacers.dp4} 0`} />
                     </>
                 )}
-                {appDetails.appType === 'APP' ? (
-                    // modern plugins
-                    <Plugin
-                        pluginSource={iframeSrc}
-                        dashboardItemId={item.id}
-                        dashboardItemFilters={itemFilters}
-                        cacheId={`${dashboardId}-${item.id}`}
-                        isParentCached={isCached}
-                    />
-                ) : (
-                    // legacy widgets
-                    <iframe
-                        title={appDetails.name}
-                        src={iframeSrc}
-                        className={cx(styles.content, {
-                            [styles.hiddenTitle]: hideTitle,
-                            [styles.fullscreen]: isFullscreen,
-                        })}
-                        style={{ border: 'none' }}
-                    />
-                )}
+                <div className="dashboard-item-content" ref={contentRef}>
+                    {renderPlugin(iframeSrc)}
+                </div>
             </>
         )
     } else {
@@ -89,6 +128,7 @@ AppItem.propTypes = {
     isFullscreen: PropTypes.bool,
     item: PropTypes.object,
     itemFilters: PropTypes.object,
+    windowDimensions: PropTypes.object,
 }
 
 const mapStateToProps = (state, ownProps) => {
