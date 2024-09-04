5f217df4c8e26fc12b3d481c4a23d0e930a6e5f2
Edoardo Sabadelli <edoardo@dhis2.org>
2024-09-04 15:45:46 +0200

    feat: enable View fullscreen and Open in for custom plugin items

--- src/components/Item/AppItem/Item.js
+++ src/components/Item/AppItem/Item.js
@@ -1,42 +1,61 @@
 // eslint-disable-next-line import/no-unresolved
 import { Plugin } from '@dhis2/app-runtime/experimental'
-import { Divider, spacers } from '@dhis2/ui'
+import i18n from '@dhis2/d2-i18n'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React, { useMemo, useRef } from 'react'
-import { connect, useSelector } from 'react-redux'
-import { EDIT, isEditMode } from '../../../modules/dashboardModes.js'
+import React, { useMemo, useReducer, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import {
+    EDIT,
+    isEditMode,
+    isViewMode,
+} from '../../../modules/dashboardModes.js'
 import { useCacheableSection } from '../../../modules/useCacheableSection.js'
 import {
     sGetItemFiltersRoot,
     DEFAULT_STATE_ITEM_FILTERS,
 } from '../../../reducers/itemFilters.js'
 import { sGetSelectedId } from '../../../reducers/selected.js'
+import FatalErrorBoundary from '../FatalErrorBoundary.js'
+import { isFullscreenSupported } from '../fullscreenUtil.js'
 import { getAvailableDimensions } from '../getAvailableDimensions.js'
 import ItemHeader from '../ItemHeader/ItemHeader.js'
 import MissingPluginMessage from '../ItemMessage/MissingPluginMessage.js'
 import { getIframeSrc } from './getIframeSrc.js'
+import { ItemContextMenu } from './ItemContextMenu.js'
 import styles from './styles/AppItem.module.css'
 
-const AppItem = ({ dashboardMode, windowDimensions, item, itemFilters, apps, isFullscreen }) => {
+const AppItem = ({ dashboardMode, windowDimensions, item, apps, sortIndex, isFullscreen }) => {
     const contentRef = useRef()
     const headerRef = useRef()
     const dashboardId = useSelector(sGetSelectedId)
+    let itemFilters = useSelector(sGetItemFiltersRoot)
+
+    if (isEditMode(dashboardMode)) {
+        itemFilters = DEFAULT_STATE_ITEM_FILTERS
+    }
 
     const { isCached } = useCacheableSection(dashboardId)
 
-    let appDetails
+    const [loadItemFailed, setLoadItemFailed] = useState(false)
 
-    const appKey = item.appKey
+    const appDetails =
+        item?.appKey && apps.find((app) => app.key === item.appKey)
 
-    if (appKey) {
-        appDetails = apps.find((app) => app.key === appKey)
-    }
+    const [{ itemTitle, appUrl }, setItemDetails] = useReducer(
+        (state, newState) => ({
+            ...state,
+            ...newState,
+            appUrl: `${appDetails?.launchUrl}${newState.appUrl}`, // hack, should the plugin send an absolute URL?!
+        }),
+        { itemTitle: appDetails?.name, appUrl: appDetails?.launchUrl }
+    )
 
     const pluginProps = useMemo(
         () => ({
             dashboardItemId: item.id,
             dashboardItemFilters: itemFilters,
+            setDashboardItemDetails: setItemDetails,
             cacheId: `${dashboardId}-${item.id}`,
             isParentCached: isCached,
         }),
@@ -63,7 +82,7 @@ const AppItem = ({ dashboardMode, windowDimensions, item, itemFilters, apps, isF
                   })
                 : {}
 
-        return appDetails.appType === 'APP' ? (
+        return appDetails?.appType === 'APP' ? (
             // modern plugins
             <Plugin
                 pluginSource={iframeSrc}
@@ -90,26 +109,45 @@ const AppItem = ({ dashboardMode, windowDimensions, item, itemFilters, apps, isF
         )
     }
 
+    const onFatalError = () => {
+        setLoadItemFailed(true)
+    }
+
     if (appDetails) {
         const iframeSrc = getIframeSrc(appDetails, item, itemFilters)
 
+        const actionButtons =
+            appDetails.pluginLaunchUrl && isViewMode(dashboardMode) ? (
+                <ItemContextMenu
+                    item={item}
+                    appName={appDetails.name}
+                    appUrl={appUrl}
+                    enterFullscreen={() => setSlideshow(sortIndex)}
+                    fullscreenSupported={isFullscreenSupported(item.id)}
+                    loadItemFailed={loadItemFailed}
+                />
+            ) : null
+
         return (
             <>
-                {!hideTitle && (
-                    <>
-                        <ItemHeader
-                            ref={headerRef}
-                            title={appDetails.name}
-                            itemId={item.id}
-                            dashboardMode={dashboardMode}
-                            isShortened={item.shortened}
-                        />
-                        <Divider margin={`0 0 ${spacers.dp4} 0`} />
-                    </>
-                )}
-                <div className="dashboard-item-content" ref={contentRef}>
-                    {renderPlugin(iframeSrc)}
-                </div>
+                <ItemHeader
+                    ref={headerRef}
+                    title={hideTitle ? '' : itemTitle}
+                    actionButtons={actionButtons}
+                    itemId={item.id}
+                    dashboardMode={dashboardMode}
+                    isShortened={item.shortened}
+                />
+                <FatalErrorBoundary
+                    message={i18n.t(
+                        'There was a problem loading this dashboard item'
+                    )}
+                    onFatalError={onFatalError}
+                >
+                    <div className="dashboard-item-content" ref={contentRef}>
+                        {renderPlugin(iframeSrc)}
+                    </div>
+                </FatalErrorBoundary>
             </>
         )
     } else {
@@ -127,16 +165,8 @@ AppItem.propTypes = {
     dashboardMode: PropTypes.string,
     isFullscreen: PropTypes.bool,
     item: PropTypes.object,
-    itemFilters: PropTypes.object,
+    sortIndex: PropTypes.number,
     windowDimensions: PropTypes.object,
 }
 
-const mapStateToProps = (state, ownProps) => {
-    const itemFilters = !isEditMode(ownProps.dashboardMode)
-        ? sGetItemFiltersRoot(state)
-        : DEFAULT_STATE_ITEM_FILTERS
-
-    return { itemFilters }
-}
-
-export default connect(mapStateToProps)(AppItem)
+export default AppItem
