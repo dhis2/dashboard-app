d95355abf5c281b4297c1bd9d161ee9c417b6df0
Edoardo Sabadelli <edoardo@dhis2.org>
2024-10-31 16:05:11 +0100

    fix: fix URL to App management app

--- src/components/Item/ItemMessage/MissingPluginMessage.js
+++ src/components/Item/ItemMessage/MissingPluginMessage.js
@@ -1,3 +1,4 @@
+import { useConfig } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
 import { Cover, IconWarning24, colors } from '@dhis2/ui'
 import PropTypes from 'prop-types'
@@ -6,6 +7,8 @@ import { isPrintMode } from '../../../modules/dashboardModes.js'
 import classes from './styles/ItemMessage.module.css'
 
 const MissingPluginMessage = ({ dashboardMode, pluginName }) => {
+    const { baseUrl } = useConfig()
+
     return (
         <Cover>
             <div className={classes.messageContent}>
@@ -21,7 +24,7 @@ const MissingPluginMessage = ({ dashboardMode, pluginName }) => {
                             onClick={(e) => e.stopPropagation()}
                             target="_blank"
                             rel="noopener noreferrer"
-                            href="/dhis-web-app-management/index.html#/app-hub"
+                            href={`${baseUrl}/dhis-web-app-management/index.html#/app-hub`}
                         >
                             {i18n.t(
                                 'Install the {{pluginName}} plugin from the App Hub',
