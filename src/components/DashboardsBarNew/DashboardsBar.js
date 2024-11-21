1e41162b85f3f4739ade5493ac44a0dad125f3ab
HendrikThePendric <hendrik@dhis2.org>
2024-11-21 17:56:00 +0100

    chore: cleanup mockup code from dashboards bar

--- src/components/DashboardsBarNew/DashboardsBar.js
+++ src/components/DashboardsBarNew/DashboardsBar.js
@@ -1,15 +1,5 @@
 import i18n from '@dhis2/d2-i18n'
-import {
-    Button,
-    IconAdd16,
-    IconStar16,
-    // IconStarFilled16,
-    IconFilter16,
-    IconMore16,
-    DropdownButton,
-    Tag,
-} from '@dhis2/ui'
-import PropTypes from 'prop-types'
+import { Button, IconAdd16, DropdownButton } from '@dhis2/ui'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
@@ -18,7 +8,7 @@ import InformationBlock from './InformationBlock/InformationBlock.js'
 import { IconNavigation, NavigationMenu } from './NavigationMenu/index.js'
 import styles from './styles/DashboardsBar.module.css'
 
-export const DashboardsBar = ({ external, offline }) => {
+export const DashboardsBar = () => {
     const history = useHistory()
     const selectedDashboard = useSelector(sGetSelected)
     console.log(selectedDashboard)
@@ -41,40 +31,6 @@ export const DashboardsBar = ({ external, offline }) => {
                 </DropdownButton>
             </div>
             <InformationBlock />
-            <div className={styles.blockInformation}>
-                <div className={styles.informationTitle}>
-                    <span className={styles.dashboardTitle}>
-                        {selectedDashboard.displayName}
-                    </span>
-                    <button className={styles.actionButtonStar}>
-                        <IconStar16 />
-                    </button>
-                    <div className={styles.tagContainer}>
-                        {offline && <Tag>Synced 3 weeks ago</Tag>}
-                        {external && <Tag>External data</Tag>}
-                    </div>
-                </div>
-                <div className={styles.informationActions}>
-                    <Button secondary small>
-                        Edit
-                    </Button>
-                    <Button secondary small>
-                        Share
-                    </Button>
-                    <Button secondary small>
-                        Slideshow
-                    </Button>
-                    <DropdownButton secondary small icon={<IconFilter16 />}>
-                        Filter
-                    </DropdownButton>
-                    <DropdownButton secondary small icon={<IconMore16 />} />
-                </div>
-            </div>
         </div>
     )
-}
-
-DashboardsBar.propTypes = {
-    external: PropTypes.bool,
-    offline: PropTypes.bool,
 }
