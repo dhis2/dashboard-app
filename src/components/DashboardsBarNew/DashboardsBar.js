ea058976e0834220fd0a970df6a8d2c9b078823a
HendrikThePendric <hendrik@dhis2.org>
2024-11-26 11:33:49 +0100

    chore: clean up dashboards-bar code

--- src/components/DashboardsBarNew/DashboardsBar.js
+++ src/components/DashboardsBarNew/DashboardsBar.js
@@ -1,17 +1,13 @@
 import i18n from '@dhis2/d2-i18n'
 import { Button, IconAdd16, DropdownButton } from '@dhis2/ui'
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
-import { sGetSelected } from '../../reducers/selected.js'
 import InformationBlock from './InformationBlock/InformationBlock.js'
 import { IconNavigation, NavigationMenu } from './NavigationMenu/index.js'
 import styles from './styles/DashboardsBar.module.css'
 
 export const DashboardsBar = () => {
     const history = useHistory()
-    const selectedDashboard = useSelector(sGetSelected)
-    console.log(selectedDashboard)
     return (
         <div className={styles.toolbar}>
             <div className={styles.blockCreationNavigation}>
