56eb0ac0ceec5c1563fe7929d46bd1f19a794030
HendrikThePendric <hendrik@dhis2.org>
2024-11-26 11:49:30 +0100

    Revert "refactor: split up actions-bar and more-menu"
    This reverts commit 75fd9f19e79833466873c4d108171cb1f46c4ab7.

--- src/components/DashboardsBarNew/InformationBlock/ActionsBar.js
+++ src/components/DashboardsBarNew/InformationBlock/ActionsBar.js
@@ -1,38 +1,180 @@
 import { OfflineTooltip } from '@dhis2/analytics'
 import { useDhis2ConnectionStatus } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
-import { Button, colors, IconMore16, SharingDialog } from '@dhis2/ui'
+import {
+    Button,
+    FlyoutMenu,
+    colors,
+    IconMore16,
+    SharingDialog,
+} from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React, { useCallback, useState } from 'react'
 import { connect } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Redirect } from 'react-router-dom'
+import { acClearItemFilters } from '../../../actions/itemFilters.js'
+import { acSetShowDescription } from '../../../actions/showDescription.js'
+import { apiPostShowDescription } from '../../../api/description.js'
+import { useCacheableSection } from '../../../modules/useCacheableSection.js'
 import { orObject } from '../../../modules/util.js'
+import { ROUTE_START_PATH } from '../../../pages/start/index.js'
+import { sGetNamedItemFilters } from '../../../reducers/itemFilters.js'
 import { sGetSelected } from '../../../reducers/selected.js'
+import { sGetShowDescription } from '../../../reducers/showDescription.js'
+import ConfirmActionDialog from '../../ConfirmActionDialog.js'
 import DropdownButton from '../../DropdownButton/DropdownButton.js'
+import MenuItem from '../../MenuItemWithTooltip.js'
 import FilterSelector from './FilterSelector.js'
-import MoreOptionsMenu from './MoreOptionsMenu.js'
 import classes from './styles/ActionsBar.module.css'
 
-const ActionsBar = ({
-    access,
-    allowedFilters,
+const ViewActions = ({
     id,
-    restrictFilters,
-    showAlert,
+    access,
+    showDescription,
     starred,
     toggleDashboardStarred,
+    showAlert,
+    updateShowDescription,
+    removeAllFilters,
+    restrictFilters,
+    allowedFilters,
+    filtersLength,
 }) => {
     const history = useHistory()
     const [moreOptionsIsOpen, setMoreOptionsIsOpen] = useState(false)
     const [sharingDialogIsOpen, setSharingDialogIsOpen] = useState(false)
+    const [confirmCacheDialogIsOpen, setConfirmCacheDialogIsOpen] =
+        useState(false)
+    const [redirectUrl, setRedirectUrl] = useState(null)
     const { isDisconnected: offline } = useDhis2ConnectionStatus()
-    const onToggleSharingDialog = useCallback(() => {
-        setSharingDialogIsOpen((currentOpenState) => !currentOpenState)
-    }, [])
-    const closeMoreOptionsMenu = useCallback(() => {
+    const { lastUpdated, isCached, startRecording, remove } =
+        useCacheableSection(id)
+
+    const onRecordError = useCallback(() => {
+        showAlert({
+            msg: i18n.t(
+                "The dashboard couldn't be made available offline. Try again."
+            ),
+            isCritical: true,
+        })
+    }, [showAlert])
+
+    const onCacheDashboardConfirmed = useCallback(() => {
+        setConfirmCacheDialogIsOpen(false)
+        removeAllFilters()
+        startRecording({
+            onError: onRecordError,
+        })
+    }, [onRecordError, removeAllFilters, startRecording])
+
+    const onRemoveFromOffline = useCallback(() => {
         setMoreOptionsIsOpen(false)
-    }, [])
+        lastUpdated && remove()
+    }, [lastUpdated, remove])
+
+    const onAddToOffline = useCallback(() => {
+        setMoreOptionsIsOpen(false)
+        return filtersLength
+            ? setConfirmCacheDialogIsOpen(true)
+            : startRecording({
+                  onError: onRecordError,
+              })
+    }, [filtersLength, onRecordError, startRecording])
+
+    const onToggleShowDescription = useCallback(() => {
+        updateShowDescription(!showDescription)
+        setMoreOptionsIsOpen(false)
+        !offline && apiPostShowDescription(!showDescription)
+    }, [offline, showDescription, updateShowDescription])
+
+    const onToggleSharingDialog = useCallback(
+        () => setSharingDialogIsOpen(!sharingDialogIsOpen),
+        [sharingDialogIsOpen]
+    )
+
     const userAccess = orObject(access)
+
+    const getMoreMenu = () => (
+        <FlyoutMenu>
+            {lastUpdated ? (
+                <MenuItem
+                    dense
+                    disabledWhenOffline={false}
+                    label={i18n.t('Remove from offline storage')}
+                    onClick={onRemoveFromOffline}
+                />
+            ) : (
+                <MenuItem
+                    dense
+                    disabled={offline}
+                    label={i18n.t('Make available offline')}
+                    onClick={onAddToOffline}
+                />
+            )}
+            {lastUpdated && (
+                <MenuItem
+                    dense
+                    label={i18n.t('Sync offline data now')}
+                    disabled={offline}
+                    onClick={onAddToOffline}
+                />
+            )}
+            <MenuItem
+                dense
+                disabled={offline}
+                label={
+                    starred
+                        ? i18n.t('Unstar dashboard')
+                        : i18n.t('Star dashboard')
+                }
+                onClick={toggleDashboardStarred}
+            />
+            <MenuItem
+                dense
+                disabledWhenOffline={false}
+                label={
+                    showDescription
+                        ? i18n.t('Hide description')
+                        : i18n.t('Show description')
+                }
+                onClick={onToggleShowDescription}
+            />
+            <MenuItem
+                dense
+                disabled={offline && !isCached}
+                disabledWhenOffline={false}
+                label={i18n.t('Print')}
+                dataTest="print-menu-item"
+            >
+                <MenuItem
+                    dense
+                    disabled={offline && !isCached}
+                    disabledWhenOffline={false}
+                    label={i18n.t('Dashboard layout')}
+                    onClick={() => setRedirectUrl(`${id}/printlayout`)}
+                    dataTest="print-layout-menu-item"
+                />
+                <MenuItem
+                    dense
+                    disabled={offline && !isCached}
+                    disabledWhenOffline={false}
+                    label={i18n.t('One item per page')}
+                    onClick={() => setRedirectUrl(`${id}/printoipp`)}
+                    dataTest="print-oipp-menu-item"
+                />
+            </MenuItem>
+            <MenuItem
+                dense
+                disabledWhenOffline={false}
+                label={i18n.t('Close dashboard')}
+                onClick={() => history.push(ROUTE_START_PATH)}
+            />
+        </FlyoutMenu>
+    )
+
+    if (redirectUrl) {
+        return <Redirect to={redirectUrl} />
+    }
 
     return (
         <>
@@ -44,7 +186,7 @@ const ActionsBar = ({
                             small
                             disabled={offline}
                             className={classes.editButton}
-                            onClick={() => history.push(`${id}/edit`)}
+                            onClick={() => setRedirectUrl(`${id}/edit`)}
                         >
                             {i18n.t('Edit')}
                         </Button>
@@ -75,14 +217,7 @@ const ActionsBar = ({
                     disabledWhenOffline={false}
                     onClick={() => setMoreOptionsIsOpen(!moreOptionsIsOpen)}
                     icon={<IconMore16 color={colors.grey700} />}
-                    component={
-                        <MoreOptionsMenu
-                            close={closeMoreOptionsMenu}
-                            showAlert={showAlert}
-                            toggleDashboardStarred={toggleDashboardStarred}
-                            starred={starred}
-                        />
-                    }
+                    component={getMoreMenu()}
                 >
                     <wbr />
                 </DropdownButton>
@@ -94,29 +229,46 @@ const ActionsBar = ({
                     onClose={onToggleSharingDialog}
                 />
             )}
+            <ConfirmActionDialog
+                title={i18n.t('Clear dashboard filters?')}
+                message={i18n.t(
+                    "A dashboard's filters can’t be saved offline. Do you want to remove the filters and make this dashboard available offline?"
+                )}
+                cancelLabel={i18n.t('No, cancel')}
+                confirmLabel={i18n.t('Yes, clear filters and sync')}
+                onConfirm={onCacheDashboardConfirmed}
+                onCancel={() => setConfirmCacheDialogIsOpen(false)}
+                open={confirmCacheDialogIsOpen}
+            />
         </>
     )
 }
 
-ActionsBar.propTypes = {
+ViewActions.propTypes = {
     access: PropTypes.object,
     allowedFilters: PropTypes.array,
+    filtersLength: PropTypes.number,
     id: PropTypes.string,
+    removeAllFilters: PropTypes.func,
     restrictFilters: PropTypes.bool,
     showAlert: PropTypes.func,
+    showDescription: PropTypes.bool,
     starred: PropTypes.bool,
     toggleDashboardStarred: PropTypes.func,
+    updateShowDescription: PropTypes.func,
 }
 
 const mapStateToProps = (state) => {
     const dashboard = sGetSelected(state)
 
     return {
-        access: dashboard.access,
-        allowedFilters: dashboard.allowedFilters,
-        id: dashboard.id,
-        restrictFilters: dashboard.restrictFilters,
+        ...dashboard,
+        filtersLength: sGetNamedItemFilters(state).length,
+        showDescription: sGetShowDescription(state),
     }
 }
 
-export default connect(mapStateToProps)(ActionsBar)
+export default connect(mapStateToProps, {
+    removeAllFilters: acClearItemFilters,
+    updateShowDescription: acSetShowDescription,
+})(ViewActions)
