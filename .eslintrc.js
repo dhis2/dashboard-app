d25c054af1d4fc29b9a26b5d2df960f1c020cccb
HendrikThePendric <hendrik@dhis2.org>
2024-12-03 15:51:01 +0100

    chore: relax eslint rules for spec files so component mocking is easier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,13 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslintReact, 'plugin:cypress/recommended'],
+    overrides: [
+        {
+            files: ['src/**/*.spec.js'],
+            rules: {
+                'react/prop-types': 'off',
+                'react/display-name': 'off',
+            },
+        },
+    ],
 }
