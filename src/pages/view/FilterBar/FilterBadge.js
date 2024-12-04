d57a13960b2dff31d8bf833bcd307fbbb1e8a995
HendrikThePendric <hendrik@dhis2.org>
2024-12-04 17:08:16 +0100

    fix: adjust filter-badge so that it is only enabled when online and align tests accordingly

--- src/pages/view/FilterBar/FilterBadge.js
+++ src/pages/view/FilterBar/FilterBadge.js
@@ -6,66 +6,62 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 import { acSetActiveModalDimension } from '../../../actions/activeModalDimension.js'
-import { useCacheableSection } from '../../../modules/useCacheableSection.js'
 import { sGetSelectedId } from '../../../reducers/selected.js'
 import classes from './styles/FilterBadge.module.css'
 
-const FilterBadge = ({ dashboardId, filter, openFilterModal, onRemove }) => {
-    const { isConnected: online } = useDhis2ConnectionStatus()
-    const { isCached } = useCacheableSection(dashboardId)
+const getFilterValuesText = (values) =>
+    values.length === 1
+        ? values[0].name
+        : i18n.t('{{count}} selected', { count: values.length })
 
-    const notAllowed = !isCached && !online
-
-    const filterText = `${filter.name}: ${
-        filter.values.length > 1
-            ? i18n.t('{{count}} selected', {
-                  count: filter.values.length,
-              })
-            : filter.values[0].name
-    }`
+const FilterBadge = ({ filter, openFilterModal, onRemove }) => {
+    const { isConnected } = useDhis2ConnectionStatus()
 
     return (
-        <div className={classes.container} data-test="dashboard-filter-badge">
-            <button
-                className={cx(classes.button, classes.filterButton)}
-                onClick={() =>
-                    openFilterModal({
-                        id: filter.id,
-                        name: filter.name,
-                    })
-                }
-            >
-                {filterText}
-            </button>
-            <Tooltip
-                content={i18n.t('Cannot remove filters while offline')}
-                openDelay={200}
-                closeDelay={100}
-                className={classes.tooltip}
-            >
-                {({ onFocus, onBlur, onMouseOver, onMouseOut, ref }) => (
+        <Tooltip
+            content={i18n.t('Cannot edit filters while offline')}
+            openDelay={200}
+            closeDelay={100}
+            className={classes.tooltip}
+        >
+            {({ onFocus, onBlur, onMouseOver, onMouseOut, ref }) => (
+                <div
+                    className={classes.container}
+                    data-test="dashboard-filter-badge"
+                    onFocus={() => !isConnected && onFocus()}
+                    onBlur={() => !isConnected && onBlur()}
+                    onMouseOver={() => !isConnected && onMouseOver()}
+                    onMouseOut={() => !isConnected && onMouseOut()}
+                    ref={ref}
+                >
                     <button
-                        className={cx(classes.button, classes.removeButton, {
-                            [classes.notAllowed]: notAllowed,
-                        })}
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                        onMouseOver={() => notAllowed && onMouseOver()}
-                        onMouseOut={() => notAllowed && onMouseOut()}
-                        ref={ref}
-                        disabled={notAllowed}
+                        data-test="filter-badge-button"
+                        className={cx(classes.button, classes.filterButton)}
+                        disabled={!isConnected}
+                        onClick={() =>
+                            openFilterModal({
+                                id: filter.id,
+                                name: filter.name,
+                            })
+                        }
+                    >
+                        {filter.name}: {getFilterValuesText(filter.values)}
+                    </button>
+                    <button
+                        data-test="filter-badge-clear-button"
+                        className={cx(classes.button, classes.removeButton)}
+                        disabled={!isConnected}
                         onClick={() => onRemove(filter.id)}
                     >
                         <IconCross16 />
                     </button>
-                )}
-            </Tooltip>
-        </div>
+                </div>
+            )}
+        </Tooltip>
     )
 }
 
 FilterBadge.propTypes = {
-    dashboardId: PropTypes.string.isRequired,
     filter: PropTypes.object.isRequired,
     openFilterModal: PropTypes.func.isRequired,
     onRemove: PropTypes.func.isRequired,
