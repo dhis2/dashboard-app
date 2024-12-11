0c250e2220a7e5ad37195a845a4cf7f16b49e575
HendrikThePendric <hendrik@dhis2.org>
2024-12-11 11:45:58 +0100

    chore: add test for dahboard details failing to load

---
+++ cypress/e2e/view_errors/error_while_fetching_dashboard_details.js
@@ -0,0 +1,23 @@
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
+
+When('I type a dashboard id in the browser url that fails to load', () => {
+    console.log('TEST RUNS')
+
+    cy.intercept('**/dashboards/iMnYyBfSxmM**', {
+        statusCode: 500,
+        body: 'Oopsie!',
+    }).as('failure')
+
+    cy.visit('#/iMnYyBfSxmM')
+    cy.wait('@failure')
+})
+
+Then(
+    'a warning message is displayed stating that the dashboard could not be loaded',
+    () => {
+        cy.contains('Load dashboard failed').should('exist')
+        cy.contains(
+            'This dashboard could not be loaded. Please try again later.'
+        ).should('exist')
+    }
+)
