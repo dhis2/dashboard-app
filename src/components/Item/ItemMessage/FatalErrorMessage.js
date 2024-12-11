43bee3754f469fbf785e94ea7f2316022684c55c
Edoardo Sabadelli <edoardo@dhis2.org>
2024-12-11 10:43:56 +0100

    refactor: use same style for errors
    Remove the Cover component as it does not cover anything...

---
+++ src/components/Item/ItemMessage/FatalErrorMessage.js
@@ -0,0 +1,20 @@
+import i18n from '@dhis2/d2-i18n'
+import { IconWarning24, colors } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React from 'react'
+import classes from './styles/ItemMessage.module.css'
+
+const FatalErrorMessage = ({ message }) => {
+    return (
+        <div className={classes.messageContent}>
+            <IconWarning24 color={colors.grey500} />
+            <span>{message}</span>
+        </div>
+    )
+}
+
+FatalErrorMessage.propTypes = {
+    message: PropTypes.string,
+}
+
+export default FatalErrorMessage
