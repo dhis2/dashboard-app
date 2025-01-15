ab57d7d1262575775e615bb6dfc5986bae83b2c2
Edson Nhancale <54271761+EdsonNhancale@users.noreply.github.com>
2025-01-15 09:32:46 +0200

    refactor: update delimiter for `pe` values in iframe source URL
    Changed the delimiter for `pe` values from semicolon (`;`) to comma (`,`), aligning with the standard format for query parameters. This change improves consistency and simplifies downstream processing.
    
    Co-authored-by: Kai Vandivier <49666798+KaiVandivier@users.noreply.github.com>

--- src/components/Item/AppItem/getIframeSrc.js
+++ src/components/Item/AppItem/getIframeSrc.js
@@ -13,7 +13,7 @@ export const getIframeSrc = (appDetails, item, itemFilters) => {
 
     // Add period (pe) to iframeSrc
     if (itemFilters[FILTER_PE] && itemFilters[FILTER_PE].length) {
-        const peValues = itemFilters[FILTER_PE].map(x => x.id).join(';');
+        const peValues = itemFilters[FILTER_PE].map(x => x.id).join(',');
         iframeSrc += `&pe=${peValues}`;
     }
 
