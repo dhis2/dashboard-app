af346723dc86db8eb941c2a3855be048ad8cd6df
HendrikThePendric <hendrik@dhis2.org>
2025-01-20 08:54:58 +0100

    chore: add reminder to add tests for UUID validation

--- src/modules/useSupersetEmbeddedDashboardFieldsState.js
+++ src/modules/useSupersetEmbeddedDashboardFieldsState.js
@@ -19,6 +19,7 @@ const defaultInitialValues = {
 
 const FIELD_CHANGE = 'FIELD_CHANGE'
 const SUPERSET_FIELD_BLUR = 'SUPERSET_FIELD_BLUR'
+// TODO: ensure the reducer unit tests assert the correctness of the UUID validation
 const UUID_PATTERN =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 
