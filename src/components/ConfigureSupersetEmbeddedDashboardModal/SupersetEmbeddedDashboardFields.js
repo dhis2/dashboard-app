471bb1461675f1fc0bd4963abce4f9268e3dbca2
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2025-01-20 12:42:40 +0100

    feat: create embedded superset dashboard (#3185)
    * chore: ignore aider ai files
    
    * refactor: extract CreateDashboardButton and NavigationMenuDropdownButton components
    
    * fix: read system settings from nested object in hooks
    
    * feat: creation flow with conditional support for superset embedded dashboards
    
    * chore: remove unused import
    
    * feat: add form that can be used for creating and editing superset embedded dashboards
    
    * chore: add file extensions to imports
    
    * chore: addjust formatting
    
    * feat: create superset embedded dashboards
    
    * fix: complete hook dependencies
    
    * fix: consolidate keyboard navigation implementation across modal forms
    
    * fix: adjust modal form, radio card styles
    
    * chore: add reminder to add tests for UUID validation
    
    * fix: set focus on first fiel in modal form instead of setting focus to fieldset
    
    * fix: align radio card outline visibility with other inputs
    
    * chore: reorganise files
    
    ---------
    
    Co-authored-by: Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>

---
+++ src/components/ConfigureSupersetEmbeddedDashboardModal/SupersetEmbeddedDashboardFields.js
@@ -0,0 +1,102 @@
+import i18n from '@dhis2/d2-i18n'
+import { CheckboxField, InputField, TextAreaField } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { fieldNames } from '../../modules/useSupersetEmbeddedDashboardFieldsState.js'
+import styles from './styles/SupersetEmbeddedDashboardFields.module.css'
+
+export const SupersetEmbeddedDashboardFields = ({
+    values,
+    submitting,
+    onChange,
+    onSupersetEmbedIdFieldBlur,
+    isSupersetEmbedIdValid,
+    isSupersetEmbedIdFieldTouched,
+}) => {
+    const supersetEmbedIdFieldHasError =
+        isSupersetEmbedIdFieldTouched && !isSupersetEmbedIdValid
+    return (
+        <>
+            <InputField
+                initialFocus
+                label={i18n.t('Title')}
+                type="text"
+                onChange={onChange}
+                value={values.title}
+                disabled={submitting}
+                name={fieldNames.title}
+                className={styles.textField}
+            />
+            <InputField
+                label={i18n.t('Code')}
+                type="text"
+                onChange={onChange}
+                value={values.code}
+                disabled={submitting}
+                name={fieldNames.code}
+                className={styles.textField}
+            />
+            <TextAreaField
+                label={i18n.t('Description')}
+                type="text"
+                onChange={onChange}
+                value={values.description}
+                disabled={submitting}
+                name={fieldNames.description}
+                className={styles.textField}
+            />
+            <InputField
+                label={i18n.t('Superset Embed ID')}
+                type="text"
+                onChange={onChange}
+                onBlur={onSupersetEmbedIdFieldBlur}
+                value={values.supersetEmbedId}
+                disabled={submitting}
+                name={fieldNames.supersetEmbedId}
+                error={supersetEmbedIdFieldHasError}
+                validationText={
+                    supersetEmbedIdFieldHasError
+                        ? i18n.t('Invalid UUID')
+                        : undefined
+                }
+                required
+                className={styles.textField}
+            />
+            <fieldset className={styles.options}>
+                <legend>{i18n.t('Options')}</legend>
+                <CheckboxField
+                    dense
+                    label={i18n.t('Show chart controls on dashboard items')}
+                    onChange={onChange}
+                    checked={values.showChartControls}
+                    disabled={submitting}
+                    name={fieldNames.showChartControls}
+                />
+                <CheckboxField
+                    dense
+                    label={i18n.t('Show filters')}
+                    onChange={onChange}
+                    checked={values.showFilters}
+                    disabled={submitting}
+                    name={fieldNames.showFilters}
+                />
+            </fieldset>
+        </>
+    )
+}
+
+SupersetEmbeddedDashboardFields.propTypes = {
+    isSupersetEmbedIdFieldTouched: PropTypes.bool,
+    isSupersetEmbedIdValid: PropTypes.bool,
+    submitting: PropTypes.bool,
+    values: PropTypes.shape({
+        code: PropTypes.string,
+        description: PropTypes.string,
+        showChartControls: PropTypes.bool,
+        showFilters: PropTypes.bool,
+        supersetEmbedId: PropTypes.string,
+        title: PropTypes.string,
+    }),
+    onChange: PropTypes.func,
+    onSupersetEmbedIdFieldBlur: PropTypes.func,
+}
---
+++ src/components/DashboardsBar/CreateDashboardButton/IconDashboardExternal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const IconDashboardExternal = () => (
+    <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M19.8789 17.1211L25.2578 22.5L3 22.5L3 25.5L25.2578 25.5L19.879 30.8789L22 33L31 24L22 15L19.8789 17.1211Z" />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9 9H39V39H9V31H6V42H42V6H6V17H9V9Z"
+        />
+    </svg>
+)
