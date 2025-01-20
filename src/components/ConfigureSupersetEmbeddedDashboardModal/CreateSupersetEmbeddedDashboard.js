7bd1d702c0d8f9a0836bbe41f56a6d1436ee108e
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2025-01-20 12:42:40 +0100

    feat: create embedded superset dashboard (#3185)
    * chore: ignore aider ai files
    
    * refactor: extract CreateDashboardButton and NavigationMenuDropdownButton components
    
    * fix: read system settings from nested object in hooks
    
    * feat: creation flow with conditional support for superset embedded dashboards
    
    * chore: remove unused import
    
    * feat: add form that can be used for creating and editing superset embedded dashboards
    
    * chore: add file extensions to imports
    
    * chore: addjust formatting
    
    * feat: create superset embedded dashboards
    
    * fix: complete hook dependencies
    
    * fix: consolidate keyboard navigation implementation across modal forms
    
    * fix: adjust modal form, radio card styles
    
    * chore: add reminder to add tests for UUID validation
    
    * fix: set focus on first fiel in modal form instead of setting focus to fieldset
    
    * fix: align radio card outline visibility with other inputs
    
    * chore: reorganise files
    
    ---------
    
    Co-authored-by: Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
    # Conflicts:
    #	i18n/en.pot

---
+++ src/components/ConfigureSupersetEmbeddedDashboardModal/CreateSupersetEmbeddedDashboard.js
@@ -0,0 +1,130 @@
+import { useDataMutation } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import {
+    Button,
+    Modal,
+    ModalActions,
+    ModalContent,
+    ModalTitle,
+    NoticeBox,
+} from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React, { useCallback, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { tFetchDashboards } from '../../actions/dashboards.js'
+import { useSupersetEmbeddedDashboardFieldsState } from '../../modules/useSupersetEmbeddedDashboardFieldsState.js'
+import styles from './styles/CreateSupersetEmbeddedDashboard.module.css'
+import { SupersetEmbeddedDashboardFields } from './SupersetEmbeddedDashboardFields.js'
+
+const postDashboardQuery = {
+    resource: 'dashboards',
+    type: 'create',
+    data: ({ values }) => ({
+        name: values.title || 'Untitled dashboard',
+        description: values.description,
+        code: values.code,
+        embedded: {
+            provider: 'SUPERSET',
+            id: values.supersetEmbedId,
+            options: {
+                hideTab: false,
+                hideChartControls: !values.showChartControls,
+                filters: {
+                    visible: values.showFilters,
+                    expanded: false,
+                },
+            },
+        },
+    }),
+}
+
+export const CreateSupersetEmbeddedDashboard = ({
+    backToChooseDashboardModal,
+    closeModal,
+}) => {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const [loading, setLoading] = useState(false)
+    const [postDashboard, { error }] = useDataMutation(postDashboardQuery)
+    const {
+        hasFieldChanges,
+        isSupersetEmbedIdValid,
+        isSupersetEmbedIdFieldTouched,
+        values,
+        onChange,
+        onSupersetEmbedIdFieldBlur,
+    } = useSupersetEmbeddedDashboardFieldsState()
+    const handleSubmit = useCallback(
+        async (event) => {
+            event.preventDefault()
+            setLoading(true)
+            const { response } = await postDashboard({ values })
+            await dispatch(tFetchDashboards())
+            closeModal()
+            history.push(`/${response.uid}`)
+        },
+        [values, postDashboard, closeModal, dispatch, history]
+    )
+
+    return (
+        <Modal>
+            <form onSubmit={handleSubmit}>
+                <ModalTitle>
+                    {i18n.t(
+                        'New dashboard: configure external source (superset)',
+                        { nsSeparator: '###' }
+                    )}
+                </ModalTitle>
+                <ModalContent>
+                    <SupersetEmbeddedDashboardFields
+                        isSupersetEmbedIdValid={isSupersetEmbedIdValid}
+                        isSupersetEmbedIdFieldTouched={
+                            isSupersetEmbedIdFieldTouched
+                        }
+                        values={values}
+                        onChange={onChange}
+                        onSupersetEmbedIdFieldBlur={onSupersetEmbedIdFieldBlur}
+                        submitting={loading}
+                    />
+                    {error && (
+                        <NoticeBox
+                            error
+                            title={i18n.t('Could not create dashboard')}
+                        >
+                            {error?.details?.response?.errorReports[0]
+                                ?.message ??
+                                i18n.t('An unknown error occurred')}
+                        </NoticeBox>
+                    )}
+                </ModalContent>
+                <ModalActions>
+                    <div className={styles.buttonStrip}>
+                        <Button
+                            loading={loading}
+                            type="submit"
+                            primary
+                            disabled={
+                                !hasFieldChanges || !isSupersetEmbedIdValid
+                            }
+                        >
+                            {i18n.t('Save dashboard')}
+                        </Button>
+                        <Button
+                            disabled={loading}
+                            secondary
+                            onClick={backToChooseDashboardModal}
+                        >
+                            {i18n.t('Back')}
+                        </Button>
+                    </div>
+                </ModalActions>
+            </form>
+        </Modal>
+    )
+}
+
+CreateSupersetEmbeddedDashboard.propTypes = {
+    backToChooseDashboardModal: PropTypes.func,
+    closeModal: PropTypes.func,
+}
