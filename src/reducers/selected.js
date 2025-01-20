1b7ed11be4f6982e1acfd4b0662119f917fba8bd
HendrikThePendric <hendrik@dhis2.org>
2025-01-20 14:06:26 +0100

    fix: ensure embedded property is on the selected dashboard state

--- src/reducers/selected.js
+++ src/reducers/selected.js
@@ -12,6 +12,7 @@ const SELECTED_PROPERTIES = {
     dashboardItems: [],
     layout: [],
     itemConfig: {},
+    embedded: undefined,
 }
 
 export default (state = DEFAULT_SELECTED_STATE, action) => {
@@ -36,6 +37,8 @@ export default (state = DEFAULT_SELECTED_STATE, action) => {
 export const sGetSelected = (state) => state.selected
 
 export const sGetSelectedId = (state) => sGetSelected(state).id
+
+export const sGetSelectedIsEmbedded = (state) => !!sGetSelected(state).embedded
 
 export const sGetSelectedDisplayName = (state) =>
     sGetSelected(state).displayName
