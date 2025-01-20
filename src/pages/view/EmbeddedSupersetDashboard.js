e30525fe7bae4cd0d23eee5ba9de2136e965b913
HendrikThePendric <hendrik@dhis2.org>
2025-01-20 14:07:56 +0100

    feat: show embedded-superset-dashboard and allow it to take up the full page

---
+++ src/pages/view/EmbeddedSupersetDashboard.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './styles/EmbeddedSupersetDashboard.module.css'
+
+export const EmbeddedSupersetDashboard = () => {
+    return <div className={styles.container}>Embedded dashboard content</div>
+}
