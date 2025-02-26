524612355a91ac751863d8c78ef4c036838c37e6
HendrikThePendric <hendrik@dhis2.org>
2025-02-26 11:26:06 +0100

    chore: adjust choose embedded dashboard type text

--- src/components/DashboardsBar/CreateDashboardButton/ChooseDashboardTypeModal.js
+++ src/components/DashboardsBar/CreateDashboardButton/ChooseDashboardTypeModal.js
@@ -55,9 +55,7 @@ export const ChooseDashboardTypeModal = ({
                             checked={selectedType === TYPE_SUPERSET}
                             onChange={handleDashboardTypeChange}
                             title={i18n.t('External')}
-                            subtitle={i18n.t(
-                                'Embed a dashboard from a third-party source, like Superset.'
-                            )}
+                            subtitle={i18n.t('Embed a superset dashboard')}
                         />
                     </fieldset>
                 </ModalContent>
