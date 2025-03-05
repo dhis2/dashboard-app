4cf73f545bdfb3b3a406529cae9b4b602ee51128
Edoardo Sabadelli <edoardo@dhis2.org>
2025-03-05 15:24:20 +0100

    chore: cleanup commented code
    Should silence some sonarqube complaint.

--- src/components/__mocks__/InstalledAppsProvider.js
+++ src/components/__mocks__/InstalledAppsProvider.js
@@ -6,47 +6,38 @@ export const InstalledAppsCtx = createContext({
     bundledApps: [],
 })
 
-//const DV_APP_KEY = 'data-visualizer'
-//const LL_APP_KEY = 'line-listing'
-//const MAPS_APP_KEY = 'maps'
-//
-//const findAppVersion = (apps, bundledApps, appKey) =>
-//    apps.find((app) => app.key === appKey)?.version ||
-//    bundledApps.find((app) => app.name === appKey)?.version ||
-//    '0.0.0'
+const appsData = {
+    apps: [
+        {
+            key: 'app-widget',
+            name: 'Scorecard',
+            appType: 'DASHBOARD_WIDGET',
+            launchUrl: 'https://iframe/app/widget/scorecard',
+        },
+        {
+            key: 'app-widget-no-title',
+            name: 'Scorecard but hide the title',
+            launchUrl: 'https://iframe/app/widget/scorecard',
+            appType: 'DASHBOARD_WIDGET',
+            settings: {
+                dashboardWidget: {
+                    hideTitle: true,
+                },
+            },
+        },
+        {
+            key: 'dashboard-plugin',
+            name: 'Dashboard plugin',
+            appType: 'APP',
+            pluginLaunchUrl: 'https://plugin/iframe/url',
+        },
+    ],
+    bundledApps: [],
+}
 
 const InstalledAppsProvider = ({ children }) => {
     return (
-        <InstalledAppsCtx.Provider
-            value={{
-                apps: [
-                    {
-                        key: 'app-widget',
-                        name: 'Scorecard',
-                        appType: 'DASHBOARD_WIDGET',
-                        launchUrl: 'https://iframe/app/widget/scorecard',
-                    },
-                    {
-                        key: 'app-widget-no-title',
-                        name: 'Scorecard but hide the title',
-                        launchUrl: 'https://iframe/app/widget/scorecard',
-                        appType: 'DASHBOARD_WIDGET',
-                        settings: {
-                            dashboardWidget: {
-                                hideTitle: true,
-                            },
-                        },
-                    },
-                    {
-                        key: 'dashboard-plugin',
-                        name: 'Dashboard plugin',
-                        appType: 'APP',
-                        pluginLaunchUrl: 'https://plugin/iframe/url',
-                    },
-                ],
-                bundledApps: [],
-            }}
-        >
+        <InstalledAppsCtx.Provider value={appsData}>
             {children}
         </InstalledAppsCtx.Provider>
     )
@@ -56,18 +47,9 @@ InstalledAppsProvider.propTypes = {
     children: PropTypes.node,
 }
 
-//const useInstalledAppVersion = (key) => {
-//    const { apps, bundledApps } = useContext(InstalledAppsCtx)
-//    return findAppVersion(apps, bundledApps, key)
-//}
-
 export default InstalledAppsProvider
 
 export const useInstalledApps = () => {
     const { apps } = useContext(InstalledAppsCtx)
     return apps
 }
-//export const useInstalledDVVersion = () => useInstalledAppVersion(DV_APP_KEY)
-//export const useInstalledLLVersion = () => useInstalledAppVersion(LL_APP_KEY)
-//export const useInstalledMapsVersion = () =>
-//    useInstalledAppVersion(MAPS_APP_KEY)
