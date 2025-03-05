799001c42c60118322b1235d89c90acead697b01
Jen Jones Arnesen <jennifer@dhis2.org>
2025-03-05 15:13:56 +0100

    fix: restore original nav menu size

--- src/components/DashboardsBar/NavigationMenu/NavigationMenu.js
+++ src/components/DashboardsBar/NavigationMenu/NavigationMenu.js
@@ -18,7 +18,7 @@ const dashboardsQuery = {
             order: 'favorite:desc,displayName:asc',
             filter: filterText ? `displayName:ilike:${filterText}` : undefined,
             paging: true,
-            pageSize: 8,
+            pageSize: 50,
             page,
         }
     },
