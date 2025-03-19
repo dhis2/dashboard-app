61e17fc80c70a745c141d04ada394c40ab5fe06a
Jen Jones Arnesen <jennifer@dhis2.org>
2025-03-19 16:40:42 +0100

    fix: hide the global header in print mode

--- src/modules/setHeaderbarVisible.js
+++ src/modules/setHeaderbarVisible.js
@@ -1,8 +1,23 @@
 export const setHeaderbarVisible = (show) => {
-    const header = document.getElementsByTagName('header')[0]
+    // Header in the current iframe
+    const iframeHeader = document.getElementsByTagName('header')[0]
+
+    // Header in the top-level document
+    const globalShellHeader = window.top.document.querySelector(
+        '.global-shell-header'
+    )
+
     if (show) {
-        header.classList.remove('hidden')
+        if (globalShellHeader) {
+            globalShellHeader.style.display = 'block'
+        } else {
+            iframeHeader?.classList.remove('hidden')
+        }
     } else {
-        header.classList.add('hidden')
+        if (globalShellHeader) {
+            globalShellHeader.style.display = 'none'
+        } else {
+            iframeHeader?.classList.add('hidden')
+        }
     }
 }
