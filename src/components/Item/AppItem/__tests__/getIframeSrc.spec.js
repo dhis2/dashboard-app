2c545865a41d85fb41c7e3f25d782d1ac8ba0e36
Jen Jones Arnesen <jennifer@dhis2.org>
2025-04-03 12:40:04 +0200

    feat: allow widgets to be filtered by period

--- src/components/Item/AppItem/__tests__/getIframeSrc.spec.js
+++ src/components/Item/AppItem/__tests__/getIframeSrc.spec.js
@@ -12,7 +12,7 @@ describe('getIframeSrc', () => {
         expect(src).toEqual(expectedSrc)
     })
 
-    it('org units chosen from the tree', () => {
+    it('org units chosen from the trees', () => {
         const ouFilter = [
             {
                 id: 'fdc6uOvgoji',
@@ -102,5 +102,77 @@ describe('getIframeSrc', () => {
         expect(src).toEqual(
             `${expectedSrc}&userOrgUnit=USER_ORGUNIT_CHILDREN,USER_ORGUNIT_GRANDCHILDREN,USER_ORGUNIT`
         )
+    })
+
+    it('only period filter with a single value', () => {
+        const peFilter = [{ id: 'LAST_MONTH' }]
+
+        const src = getIframeSrc(dashboardItem, { pe: peFilter }, appDetails)
+        expect(src).toEqual(`${expectedSrc}&period=LAST_MONTH`)
+    })
+
+    it('only period filter with multiple values', () => {
+        const peFilter = [{ id: 'LAST_MONTH' }, { id: 'LAST_3_MONTHS' }]
+
+        const src = getIframeSrc(dashboardItem, { pe: peFilter }, appDetails)
+        expect(src).toEqual(`${expectedSrc}&period=LAST_MONTH,LAST_3_MONTHS`)
+    })
+
+    it('period filter and org unit filter', () => {
+        const peFilter = [{ id: 'LAST_MONTH' }]
+        const ouFilter = [
+            {
+                id: 'fdc6uOvgoji',
+                path: '/ImspTQPwCqd/fdc6uOvgoji',
+                name: 'Bombali',
+            },
+        ]
+
+        const src = getIframeSrc(
+            dashboardItem,
+            {
+                pe: peFilter,
+                ou: ouFilter,
+            },
+            appDetails
+        )
+        expect(src).toEqual(
+            `${expectedSrc}&userOrgUnit=fdc6uOvgoji&period=LAST_MONTH`
+        )
+    })
+
+    it('period filter with multiple values and org unit filter', () => {
+        const peFilter = [{ id: 'LAST_MONTH' }, { id: 'LAST_3_MONTHS' }]
+        const ouFilter = [
+            {
+                id: 'fdc6uOvgoji',
+                path: '/ImspTQPwCqd/fdc6uOvgoji',
+                name: 'Bombali',
+            },
+            {
+                id: 'lc3eMKXaEfw',
+                path: '/ImspTQPwCqd/lc3eMKXaEfw',
+                name: 'Bonthe',
+            },
+        ]
+
+        const src = getIframeSrc(
+            dashboardItem,
+            {
+                pe: peFilter,
+                ou: ouFilter,
+            },
+            appDetails
+        )
+        expect(src).toEqual(
+            `${expectedSrc}&userOrgUnit=fdc6uOvgoji,lc3eMKXaEfw&period=LAST_MONTH,LAST_3_MONTHS`
+        )
+    })
+
+    it('empty pe filter', () => {
+        const peFilter = []
+
+        const src = getIframeSrc(dashboardItem, { pe: peFilter }, appDetails)
+        expect(src).toEqual(expectedSrc)
     })
 })
