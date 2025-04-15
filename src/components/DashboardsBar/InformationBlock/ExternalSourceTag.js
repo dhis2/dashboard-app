06237a01dd7a1aed82e90a0f5ba3bc51bc5aabf2
Edoardo Sabadelli <edoardo@dhis2.org>
2025-04-15 15:29:54 +0200

    fix: added padding around external source tag in small screen

--- src/components/DashboardsBar/InformationBlock/ExternalSourceTag.js
+++ src/components/DashboardsBar/InformationBlock/ExternalSourceTag.js
@@ -1,22 +1,34 @@
 import i18n from '@dhis2/d2-i18n'
 import { Tag, Tooltip } from '@dhis2/ui'
+import cx from 'classnames'
 import React from 'react'
-import classes from './styles/LastUpdatedTag.module.css'
+import { useSelector } from 'react-redux'
+import { sGetSelectedIsEmbedded } from '../../../reducers/selected.js'
+import classes from './styles/ExternalSourceTag.module.css'
 
-const ExternalSourceTag = () => (
-    <Tooltip
-        content={i18n.t(
-            'This dashboard is showing data from outside this system'
-        )}
-        openDelay={200}
-        closeDelay={100}
-    >
-        {(props) => (
-            <div className={classes.lastUpdatedTag} {...props}>
-                <Tag maxWidth="200px">{i18n.t('External source')}</Tag>
-            </div>
-        )}
-    </Tooltip>
-)
+const ExternalSourceTag = () => {
+    const isEmbeddedDashboard = useSelector(sGetSelectedIsEmbedded)
+
+    return (
+        <Tooltip
+            content={i18n.t(
+                'This dashboard is showing data from outside this system'
+            )}
+            openDelay={200}
+            closeDelay={100}
+        >
+            {(props) => (
+                <div
+                    className={cx(classes.externalSourceTag, {
+                        [classes.padded]: isEmbeddedDashboard,
+                    })}
+                    {...props}
+                >
+                    <Tag maxWidth="200px">{i18n.t('External source')}</Tag>
+                </div>
+            )}
+        </Tooltip>
+    )
+}
 
 export default ExternalSourceTag
