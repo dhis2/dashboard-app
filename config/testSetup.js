bbacc6c41229461add2b5ec4ac87b2733055631b
Edoardo Sabadelli <edoardo@dhis2.org>
2025-06-10 12:22:43 +0200

    chore: add link to Highcharts issue

--- config/testSetup.js
+++ config/testSetup.js
@@ -7,6 +7,7 @@ global.ResizeObserver = ResizeObserver
 
 // Emulate CSS.supports API
 // Needed with Highcharts >= 12.2.0
+// See: https://github.com/highcharts/highcharts/issues/22910
 global.CSS.supports = () => true
 
 configure({
