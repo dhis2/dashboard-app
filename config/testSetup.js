9745855a2849e68ea2d58594803814b4eb3b16fc
Edoardo Sabadelli <edoardo@dhis2.org>
2025-06-10 12:18:18 +0200

    chore: fix Jest setup to avoid issues with Highcharts

--- config/testSetup.js
+++ config/testSetup.js
@@ -1,8 +1,13 @@
 import { configure } from '@testing-library/dom'
 import '@testing-library/jest-dom'
 import ResizeObserver from 'resize-observer-polyfill'
+import 'jest-webgl-canvas-mock'
 
 global.ResizeObserver = ResizeObserver
+
+// Emulate CSS.supports API
+// Needed with Highcharts >= 12.2.0
+global.CSS.supports = () => true
 
 configure({
     testIdAttribute: 'data-test',
