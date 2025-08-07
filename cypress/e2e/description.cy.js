6d213bf40b311772b1154023685944b046ae6958
Jen Jones Arnesen <jennifer@dhis2.org>
2025-08-07 09:24:32 +0200

    chore: add comments to steps in description test

--- cypress/e2e/description.cy.js
+++ cypress/e2e/description.cy.js
@@ -5,7 +5,7 @@ const RESP_CODE_200 = 200
 const RESP_CODE_201 = 201
 
 it('toggles show description', () => {
-    // Ensure that the description is not currently shown
+    // Send request to ensure that the description is not currently shown
     cy.request({
         method: 'PUT',
         url: `${getApiBaseUrl()}/api/userDataStore/dashboard/showDescription`,
@@ -19,19 +19,19 @@ it('toggles show description', () => {
         expect(response.status).to.be.oneOf([RESP_CODE_201, RESP_CODE_200])
     })
 
-    //  Open dashboard and confirm that the description is not shown
+    // Open dashboard and confirm that the description is not shown
     cy.visit('/')
     cy.getByDataTest('view-dashboard-title').should('be.visible')
     cy.get(dashboardDescriptionSel).should('not.exist')
 
-    // intercept the request to toggle visibility to visible
+    // Intercept the request to toggle visibility to visible
     cy.intercept({
         method: 'PUT',
         pathname: '**/userDataStore/dashboard/showDescription',
         times: 1,
     }).as('toggleDescriptionVisible')
 
-    //  Click to display the description
+    // Click to display the description
     cy.getByDataTest('more-actions-button').click()
     cy.get('li').contains('Show description').click()
 
@@ -40,14 +40,14 @@ it('toggles show description', () => {
         .should('be.oneOf', [RESP_CODE_200, RESP_CODE_201])
     cy.get(dashboardDescriptionSel).should('be.visible')
 
-    // intercept the request to toggle visibility to hidden
+    // Intercept the request to toggle visibility to hidden
     cy.intercept({
         method: 'PUT',
         pathname: '**/userDataStore/dashboard/showDescription',
         times: 1,
     }).as('toggleDescriptionHidden')
 
-    //  Click to hide the description
+    // Click to hide the description
     cy.getByDataTest('more-actions-button').click()
     cy.get('li').contains('Hide description').click()
 
@@ -55,5 +55,7 @@ it('toggles show description', () => {
     cy.wait('@toggleDescriptionHidden')
         .its('response.statusCode')
         .should('be.oneOf', [RESP_CODE_200, RESP_CODE_201])
+
+    // Confirm that the description is not shown
     cy.get(dashboardDescriptionSel).should('not.exist')
 })
